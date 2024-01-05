c7359c2910166c31bd65da959ac49428188b00db
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-05 07:55:06 +0530

    Added OnithBit code

---
+++ NumberSystems/ONithBit.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ONithBit {
+    
+    /*
+     * Write a program to input a number to switch ON the ith bit in it's binary equivalent
+     * Example (n=36) = 0 0 1 0 0 1 0 0 
+     *                          i
+     *            O/P = 0 0 1 0 1 1 0 0
+     * 
+     * Approach : There are two approaches that we can follow to get the desired output,
+     * first is to convert the given number in binary and then find the ith element and
+     * make it i(ON)
+     * 
+     * Secondly we can use bit manipulation using Bitwise Operations to get the required
+     * result.
+     * 
+     * if n = 36 and i = 3
+     * then,
+     * we'll create a mask using 1 and shift left with the position of i (i.e 3)
+     * and do OR opertion with n.
+     * Automatically by doing this our ith bit will switch ON. 
+     */
+
+    static void switchOn(int n, int i){
+
+        System.out.println("Binary Equivalent of " + n + " = " + Integer.toBinaryString(n));
+        int mask = 1 << i;
+
+        System.out.println("Switching ON " + i + " position = " + Integer.toBinaryString(n | mask));
+
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n = sc.nextInt();
+        System.out.println("Enter the position you want to switch ON");
+        int i = sc.nextInt();
+
+        switchOn(n,i);
+        sc.close();
+
+    }
+
+
+}
