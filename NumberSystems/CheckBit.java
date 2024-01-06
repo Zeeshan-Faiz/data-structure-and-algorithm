34473d637823b28854f4a457aed5f50f24dbe04f
Md Zeeshan Faiz <154392974+Zeeshan-Faiz@users.noreply.github.com>
2024-01-06 15:52:26 +0530

    Added CheckBit code

--- NumberSystems/CheckBit.java
+++ NumberSystems/CheckBit.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class CheckBit {
 
-	
     /*
      * Write a program to input a number and check the ith bit is ON or OFF in it's binary equivalent
      * Example (n=36, i=5) = 0 0 1 0 0 1 0 0 
