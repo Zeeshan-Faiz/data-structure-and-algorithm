dda159a3d6f8f3bc958f67042fab129d87d5f67c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-08 22:43:00 +0530

    Added TrailingZeroesInBit code

--- NumberSystems/TrailingZeroesInBit.java
+++ NumberSystems/TrailingZeroesInBit.java
@@ -17,7 +17,7 @@ public class TrailingZeroesInBit {
      * We could observe that we need to count the number of trailing zeroes after the 
      * Right Most Set Bit (RMSB). For that we'll try to find the RMSB using the already
      * known formula ==> (n & (n-1)) = mask
-     * ==> n ^ mask = gives the position of the RMSV which in other hand will give us the
+     * ==> n ^ mask = gives the position of the RMSB which in other hand will give us the
      * number of trailing zeroes.
      * 
     */
