049ed1de81fb07b543265221c4e578cd45fad01c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-10 22:49:05 +0530

    Added PrintNnumbers code

---
+++ Recursion/PrintNnumbers.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class PrintNnumbers {
+    
+    /*
+     * Write a program to print the first N natural numbers using recursion
+     * Example n = 5
+     * O/P =  1 2 3 4 5
+     * 
+    */
+
+    /*
+     * Approach : As it's a quite simple question to print the first N natural numbers we can have printN(n) = printN(n-1)
+     * We know that Natural Number starts from 1, so our base condition will be (n == 0)
+     * 
+    */
+
+    static void printN(int n){
+
+        if(n == 0)
+            return;
+        
+        printN(n-1);
+        System.out.println(n);
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+
+        printN(n);
+
+        sc.close();
+    }
+
+
+
+}
