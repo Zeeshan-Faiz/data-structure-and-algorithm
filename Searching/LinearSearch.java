b716ed28e4dc9d185b4416f95eadc211f3bdd814
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-15 12:21:41 +0530

    Added LinearSearch code

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinearSearch {
     
     /*
@@ -6,9 +8,36 @@ public class LinearSearch {
      * 
      * Example arr = {50,10,33,40,26} ; key = 40
      * O/P = 3
-     * 
-     * 
     */
 
+    static int linearSearch(int arr[], int key){
+
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array: ");
+		int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements: ");
+        for(int i = 0; i<= arr.length-1; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter Key value to be searched : ");
+        int key = sc.nextInt();
+
+        System.out.println("Positon of key = "+ key + " in the given array is " + linearSearch(arr, key));
+        
+        sc.close();
+
+    }
 
 }
