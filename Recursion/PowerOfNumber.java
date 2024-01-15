b21b61aaf442d477b2fa5dcbc4280937bde206f6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-15 12:03:30 +0530

    Added PowerOfNumber code

--- Recursion/PowerOfNumber.java
+++ Recursion/PowerOfNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PowerOfNumber {
     
     /*
@@ -8,7 +10,7 @@ public class PowerOfNumber {
     */
 
     /*
-     * Approach 1 : We know that we can recursively solve this question starting from the given power 8 until
+     * Approach 1 : We know that we can recursively solve this question starting from the given power until
      * the power reaches 0 and at this point we'll return 1.
      * 
      * So for example if x = 5 , y = 4 then,
@@ -29,8 +31,30 @@ public class PowerOfNumber {
         return approach1(x, y-1) * x;
     }
 
+    /*
+     * Appraoch 2 : Even if we have got the correct answer with the approach 1 but it's to be noted that if the
+     * power is a bigger number like 500,1000 then in that case huge amount of stack memory will be used, so in order
+     * to tackle that we'll be trying to generate a logic such that if we have even value as a power we'll directly deduce
+     * it to half in order to reduce the stack memory by reducing the number of recursive calls.
+     * 
+     * So for example if x = 5 , y = 4 then,
+     * 5^4 = 5^2 * 5^2
+     * 
+    */
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number and it's power: ");
+		int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("Power of " + x + "^" + y + " is : " + approach1(x,y));
+
+        sc.close();
+    }
 
 
 
-    
+
 }
