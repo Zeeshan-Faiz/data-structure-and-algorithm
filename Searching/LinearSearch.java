ef3c5cf4fbfc38fa708de7247e4cea264747682b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-15 12:16:12 +0530

    Added LinearSearch code

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,14 @@
+public class LinearSearch {
+    
+    /*
+     * Write a program to input a key value and check whether this key is present in the 
+     * given array using Linear Search if yes then print it's position in the array.
+     * 
+     * Example arr = {50,10,33,40,26} ; key = 40
+     * O/P = 3
+     * 
+     * 
+    */
+
+
+}
