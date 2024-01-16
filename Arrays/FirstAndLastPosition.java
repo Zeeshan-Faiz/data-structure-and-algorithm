df83413c4e8b12be4b607b6cead4fb7f18738ced
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-16 17:58:29 +0530

    Added FirstAndLastPosition code

---
+++ Arrays/FirstAndLastPosition.java
@@ -0,0 +1,20 @@
+public class FirstAndLastPosition {
+    
+    /*
+     * Write a program to find the first and last position of a given number in a sorted array.
+     * 
+     * Example arr = {3,4,4,4,4,4,4,4,4,6}
+     * O/P = [1,8]
+     *  
+    */
+
+    /*
+     * Approach : For this particular problem we'll take an array of length 2 popualted with [-1,-1]
+     * And then run two different Binary search in order to get the first and last position of the given number
+     * in the array.
+     * Time Complexity (for 2 Binary Search) = O(Log N) + O(Log N) = O(2 Log N) ~ O(Log N)
+    */
+
+
+    
+}
