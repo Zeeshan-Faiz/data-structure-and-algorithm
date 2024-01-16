ae3b36aaab0916599ce0b5bff9dc258803f52e01
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-16 19:20:06 +0530

    Added SearchInRotatedArray code

---
+++ Arrays/SearchInRotatedArray.java
@@ -0,0 +1,26 @@
+public class SearchInRotatedArray {
+    
+    /*
+     * Given an array integer sorted in ascending order (all distinct elemenets), and then the array is rotated
+     * at an unknown pivot point. Now you need to search for a target element (key) in this rotated araay with
+     * time complexity of O(Log N)
+     * 
+     * Example original array   = {0,1,2,4,5,6,7,8}
+     * Roated array at pivot(3) = {4,5,6,7,8,0,1,2} ; key = 0
+     * O/P = 4
+    */
+
+    /*
+     * Approach : If we look closely into the rotated array we'll observe that it has two sorted arrays inside
+     * it.
+     *                     <---->
+     *          [4,5,6,7,8,0,1,2]
+     *          <-------->
+     * So we'll have two conditions to check for the given key in both the parts of the array using Binary Search.
+    */
+
+    
+
+
+
+}
