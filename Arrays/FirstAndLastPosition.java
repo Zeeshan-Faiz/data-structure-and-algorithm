e73a261af718273865297e0da84cdede11ddcfbe
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-16 18:08:20 +0530

    Added FirstAndLastPosition code

--- Arrays/FirstAndLastPosition.java
+++ Arrays/FirstAndLastPosition.java
@@ -15,6 +15,45 @@ public class FirstAndLastPosition {
      * Time Complexity (for 2 Binary Search) = O(Log N) + O(Log N) = O(2 Log N) ~ O(Log N)
     */
 
+    static int[] searchRange(int[] arr, int key){
+
+        int low = 0, high = arr.length-1, mid = 0;
+        int res[] = {-1,-1};
+
+        while(low <= high){
+
+            mid = (low+high)/2;
+            if(key == arr[mid]){ //check for the first occurence of the given number
+                res[0] = mid;
+                high = mid - 1;
+            }
+            else if(key < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        low = 0;
+        high = arr.length-1;
+        mid = 0;
+
+        while(low <= high){
+
+            mid = (low+high)/2;
+            if(key == arr[mid]){ //check for the first occurence of the given number
+                res[1] = mid;
+                low = mid + 1;
+            }
+            else if(key < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return res;
+
+    }
+
+    
 
     
 }
