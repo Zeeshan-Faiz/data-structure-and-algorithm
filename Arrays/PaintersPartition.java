aa134876a8330c8cd6b39043e1dc2ca4d2443c12
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-18 23:13:12 +0530

    Added PaintersPartition code

--- Arrays/PaintersPartition.java
+++ Arrays/PaintersPartition.java
@@ -29,11 +29,62 @@ public class PaintersPartition {
      * Maximum unit of board that can be allocate to a painter (10+20+30+40) = 100
      * Here l = 0, h = 100 and m = (l+h)/2
      * We'll be using binary search to find whether when the all the boards are allocated to all painters then the
-     * out of all maximum number of unit of boards that the painters can paint we are able to return the minimum number of unit of board.
+     * out of all maximum unit of boards that the painters can paint we are able to return the minimum number of unit of board.
      * 
      * We'll also be having painters and pbc and res as variable to store number of painters, painters board count and
      * final result respectively.
     */
+
+    static int maxTime(int[] ar, int a, int b){
+        
+        //find total number of unit of boards
+        int l = 0, h = 0;
+        for(int i = 0; i < ar.length; i++){
+            h = h + ar[i];
+        }
+
+        int res = -1;
+        while(l <= h){
+            int m = (l+h)/2;
+            if(isPossibleSol(ar,a,m)){
+                res = m;
+                h = m - 1;
+            }
+            else
+                l = m + 1;
+        }
+        return res*b;
+    }
+
+    static boolean isPossibleSol(int[] ar, int a, int m){
+
+        int painters = 1, pbc = 0;
+        for(int i = 0; i < ar.length; i++){
+
+            //check the unit of board is greater than the assumed highest unit of board
+            if(ar[i] > m)
+                return false;
+            
+            if(pbc + ar[i] <= m)
+                pbc = pbc + ar[i];
+            else{
+                painters++;
+                if(painters > a)
+                    return false;
+                
+                pbc = ar[i];
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] ar = {10,20,30,40};
+        int a = 2;
+        int b = 2;
+        System.out.println(maxTime(ar,a,b));
+    }
 
 
 }
