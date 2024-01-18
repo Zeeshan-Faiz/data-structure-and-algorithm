1005f86c75b638cd5a8697b450b7d4c99752aa96
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-18 22:30:53 +0530

    Added BookAllocation code

---
+++ Arrays/BookAllocation.java
@@ -0,0 +1,29 @@
+public class BookAllocation {
+    
+    /*
+     * You'll be given a set of books whose pages will be specified in the form of array elements. You have to allocate
+     * the books to a given number of students on the basis of below conditions:
+     * i)A book will be allocated to exactly one student.
+     * ii)Each student has to be allocated atleast one book.
+     * iii)Allotment should be in contagious order.
+     * iv)All books must be allocated.
+     * 
+     * You've to return the minimum of total number of pages out of all the combinations when the book is allocated
+     * to different students.
+     * 
+     * Example arr = {12,34,67,90} b = 2(no. of students)
+     * 
+     * Different combination of allocating the books to two students:
+     * 
+     *      Student 1                   Student 2
+     *      12,34,67 = 113 Pages        90 Pages
+     *      12,34 = 46 Pages            67,90 = 157 Pages
+     *      12 Pages                    34,67,90 = 191 Pages
+     * 
+     * Here if we need to find the min(113,157,191) = 113 which denotes that out of 3 maximum value of pages 113
+     * is the minimum.
+     * 
+    */
+
+
+}
