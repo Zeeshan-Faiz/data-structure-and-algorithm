b9f9411a960dd1e2d398e9c7419aceca18daed2e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-18 11:09:21 +0530

    Added MedianOf2SortedArray code

--- Arrays/MedianOf2SortedArray.java
+++ Arrays/MedianOf2SortedArray.java
@@ -93,15 +93,35 @@ public class MedianOf2SortedArray {
      * 2)Right most element of the left half(7) is larger than all the elements in the left half.
      * 3)Left most elements of the right half(8) is smaller than all the elements in the right half.
      * 
-     * Now we can have different combinations
-     * i)Taking 3 elements from array 1 and 2 elements from array 2
+     * Now we can have different combinations by dividing both the given array from m1 and m2 points
+     *           
+     * m1 = (l+h)/2   
+     *       l  m1   h
+     * ar1 = {1,3|8,17}
+     * 
+     * m2 = ((len(ar1) + len(ar2))+1)/2 - m1
+     * ar2 = {5,6,7,19,21,25}
+     * 
+     * 
+     * i)Taking 3 elements from array 1(m1 = 3) and 2 elements from array 2(m2 = 2)
      *  
      *      ar1 -> 3  |  ar2 -> 2
      *      1 3 8(L1) |  (R1)17
      *      5 6(L2)   |  (R2)7  19 21 25
      * 
-     * To find whether the generated combination is valid or not we'll have two conditions
+     * ii)Taking 2 elements from array 1(m1 = 2) and 3 elements from array 2(m2 = 3)
+     *  
+     *      ar1 -> 2  |  ar2 -> 2
+     *      1 3(L1)   |  (R1)8 17
+     *      5 6 7(L2) |  (R2)19 21 25
+     * 
+     * To find whether the generated combination is valid or not we'll have two conditions.
      * if(L1 <= R2) && (L2 <= R1) if yes then we got the combination which will help us find the median.
+     * 
+     * L1 = ar1[m1-1]
+     * R1 = ar1[m1]
+     * L2 = ar2[m2-1]
+     * R2 = ar2[m2]
      * 
      * For Median(even) = (max(L1,L2) + min(R1,R2))/2 
      * For Median(odd) = max(L1,L2)
@@ -114,7 +134,36 @@ public class MedianOf2SortedArray {
 
     static float findMedian2(int[] ar1, int[] ar2){
 
-        
+        //to always have the first array smaller
+        if(ar2.length < ar1.length)
+            return findMedian2(ar2, ar1);
+
+        int l = 0, h = ar1.length, m1 = 0, m2 = 0;
+        while( l <= h){
+            m1 = (l+h)/2;
+            m2 = ((ar1.length + ar2.length)+1)/2 - m1;
+
+            //check for edge cases(going out of array boundary)
+            int l1 = (m1 == 0) ? Integer.MIN_VALUE : ar1[m1 - 1];
+            int r1 = (m1 == ar1.length) ? Integer.MAX_VALUE : ar1[m1];
+
+            int l2 = (m2 == 0) ? Integer.MIN_VALUE : ar2[m2 - 1];
+            int r2 = (m2 == ar2.length) ? Integer.MAX_VALUE : ar2[m2];
+
+            if(l1 <= r2 && l2 <= r1){
+
+                //even median
+                if((ar1.length+ar2.length) %2 == 0)
+                    return (float)(Integer.max(l2, l2) + Integer.min(r1, r2)) / 2;
+                else
+                    return (float)(Integer.max(l1, l2));
+            }
+            else if(l2 > r1)
+                l = m1 + 1;
+            else
+                h = m1 - 1;
+        }
+        return 0.0f;
 
     }
 
