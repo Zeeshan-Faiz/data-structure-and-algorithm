28896234eba3ae9fa941a96209544962becec81e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-19 22:54:18 +0530

    Added RemoveDuplicates code

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -10,19 +10,36 @@ public class RemoveDuplicates {
      * Approach 1(Brute Force) : We'll be using an auxiliary array to store the array elements with no duplicates.
      * ar = {2,2,3,3,4,6,6} ; temp = {2,3,4,6}.
      * We'll be having two array indexes one i which will iterate over the given array and the other one as rd
-     * which will iterate over the temporary to store non-duplicate values.
+     * which will iterate over the temporary array to store non-duplicate values.
     */
 
-    static void approach1(int[] ar){
+    static int rd = 0;
+    static int[] approach1(int[] ar){
 
         int[] temp = new int[ar.length];
-        int rd = 0;
+        
         temp[rd] = ar[0];
         for(int i = 1; i < ar.length; i++){
             if(temp[rd] != ar[i]){
                 rd++;
                 temp[rd] = ar[i];
             }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args){
+
+        int ar[] = {0,0,2,2,3,3,4,6,6};
+        System.out.println("Array before removing duplicates :");
+        for(int i = 0; i < ar.length ; i++){
+            System.out.print(ar[i] + " ");
+        }
+
+        int[] temp = approach1(ar);
+        System.out.println("\nArray After removing duplicates :");
+        for(int i = 0; i <= rd ; i++){
+                System.out.print(temp[i] + " ");
         }
     }
 
