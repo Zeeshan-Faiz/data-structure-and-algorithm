98b0162e9ef5045c160265193543aad1d6250b66
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-19 22:32:24 +0530

    Added SquareRoot code

--- NumberSystems/SquareRoot.java
+++ NumberSystems/SquareRoot.java
@@ -1,9 +1,7 @@
 public class SquareRoot {
     
     /*
-     * Write a program to find the square root of a number. If the given number n is not perfect square then return
-     * floor(sqrt(n))
-     * 
+     * Write a program to find the square root of a number. If the given number n is not perfect square then return floor(sqrt(n))
      * Example 1 n = 25
      * O/P = 5
      * 
@@ -13,10 +11,8 @@ public class SquareRoot {
     */
 
     /*
-     * Appraoch : We know that for any given number the square root of that particular will always be less than
-     * n/2.
-     * For example square root of 16 = 4, here the number range that are a candidate to be a square root of 16 lies between
-     * 1 to (16/2) = 1 to 8
+     * Appraoch : We know that for any given number the square root of that particular will always be less than n/2.
+     * For example square root of 16 = 4, here the number range that are the candidate to be a square root of 16 lies between 1 to (16/2) = 1 to 8
      * So instead of us looping every number till n to find the square root, if we loop until n/2 will also give us the
      * value of square root of n.
      * 
@@ -32,5 +28,27 @@ public class SquareRoot {
      * as according to the question in such cases we need to return floor(sqrt(n))   
      * 
     */
+
+    static int sqrt(int n){
+
+        if(n == 1 || n == 0)
+            return n;
+
+        int l = 2, h = n/2, res = 0;
+        while(l < h){
+            int m = (l+h)/2;
+            if(m * m == n)
+                return m;
+            
+            else if(m * m < n){
+                l = m + 1;
+                res = m;
+            }
+            
+            else
+                h = m - 1;
+        }
+        return res;
+    }
 
 }
