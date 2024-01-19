5f7044bc5800d320fc702123ad13e0f441f1b4f6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-19 22:10:13 +0530

    Added CheckSorted code

--- Arrays/CheckSorted.java
+++ Arrays/CheckSorted.java
@@ -15,7 +15,14 @@ public class CheckSorted {
         return true;
     }
 
-    
+    public static void main(String[] args){
+
+        int arr[] = {2,4,6,8,10,12,14};
+
+        System.out.println("Is the given array sorted ? " + isSorted(arr));
+
+    }
+
 
 
 }
