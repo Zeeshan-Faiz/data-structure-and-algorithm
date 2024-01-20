674a2af9367ed0deb9c165b4a56283b03ae4a43a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-20 20:42:31 +0530

    Added LeadersInArray code

---
+++ Arrays/LeadersInArray.java
@@ -0,0 +1,26 @@
+public class LeadersInArray {
+    
+    /*
+     * Write a program to find the leaders of an array. An element is a leader if it is strictly greater than all 
+     * the elements to it's right side and the rightmost element is always the leader.
+     * 
+     * Example ar = {8,11,5,11,7,6,5}
+     * O/P = 11,7,6,3
+     * 
+     * Example ar2 = {50,60,70} (sorted in ascending order)
+     * O/P = 70
+     * 
+     * Example ar3 = {100,90,80}
+     * O/P = 100,90,80
+    */
+
+    /*
+     * Approach 1 (Brute Force) : We'll iterate over the whole array and try to check if the element is greater
+     * than all the right elements in the array and we'll check for this condition for all the elemnts in the array and
+     * print the leader elements only.
+     * 
+     * Time Compplexity = O(nxn)
+    */
+
+
+}
