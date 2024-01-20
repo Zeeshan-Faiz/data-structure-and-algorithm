540a76157cf42a537233fb3502baed78e42f444c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-20 19:56:08 +0530

    Added MoveZeroes code

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,20 @@
+public class MoveZeroes {
+    
+    /*
+     * Write a program to move all the zeroes to the end of an array while maintaining the relative order of the
+     * non-zero elements.
+     * 
+     * Example ar = {0,10,22,8,0,5,0}
+     * O/P = {10,22,8,5,0,0}
+    */
+
+    /*
+     * Approach : We'll be having two variables nz and z poiting to non-zero and zero elements respectively.
+     * we'll iterate over the array and if we get any 0 element we'll swtich that element by the next non-zero
+     * element.
+    */
+
+    
+
+
+}
