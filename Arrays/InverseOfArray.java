3448ece9ee05bbed6506fba9689c9d78666352b4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-20 20:28:45 +0530

    Added InverseOfArray code

---
+++ Arrays/InverseOfArray.java
@@ -0,0 +1,20 @@
+public class InverseOfArray {
+
+    /*
+     * Write a program to print the inverse of a given array. Inverse of an array is when the array indexes is placed
+     * according to it's index value or in simple statement whatever is the index becomes the value in the next array.
+     * 
+     * Example ar = {2,3,1,0,4}
+     * O/P = {3,2,0,1,4}
+    */
+
+    /*
+     * Approach : Our aim is to inverse the array, so 
+     *                          0 1 2 3 4
+     * if the given array is = {2,3,1,0,4}
+     * so the new array will contain the values whatever the index has of the first array, 
+     * suppose ar[0] = 2 then second array br[2] = 0
+     * similarly ar[1] = 3 then br[3] = 0 and so on.
+    */
+    
+}
