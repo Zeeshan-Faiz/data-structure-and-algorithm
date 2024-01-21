4dc4ea21d6d8ea0d0d95c7ab7524efe1bda93c0d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-21 15:05:07 +0530

    Added ConsecutiveOnes code

--- Arrays/ConsecutiveOnes.java
+++ Arrays/ConsecutiveOnes.java
@@ -33,13 +33,23 @@ public class ConsecutiveOnes {
      * Time Complexity = O(n)
     */
 
-    
+    static int approach2(int[] ar){
+
+        int count = 0, maxCount = 0;
+        for(int i = 0; i < ar.length; i++){
+            if(ar[i] == 0)
+                count = 0;
+            else{
+                count++;
+                maxCount = Math.max(maxCount, count);
+            }
+        }
+        return maxCount;
+    }
 
     public static void main(String[] args){
 
         int ar[] = {0,1,1,0,0,1,1,1,0};
-        System.out.println(approach1(ar));
+        System.out.println(approach2(ar));
     }
-
-
 }
