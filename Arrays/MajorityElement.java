16017b1dea0986d0310617aa31b692f8252e3ae0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-22 21:44:51 +0530

    Added MajorityElement code

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -8,7 +8,29 @@ public class MajorityElement {
      * O/P = 8
     */
 
+    /*
+     * Approach 1 (Brute Force) : We'll traverse the whole element and check the count for each element and find
+     * the maximum occurence an element and then return it accordingly.
+     * 
+     * Time Complexity = O(n^2)
+    */
+
+    static int majorityElement1(int[] ar){
+
+        for(int i = 0; i < ar.length; i++){
+            int count = 1;
+            for(int j = i=1; j < ar.length; j++){
+                if(ar[i] == ar[j])
+                    count++;
+            }
+            if(count > ar.length/2)
+                return ar[i];
+        }
+        return -1;
+    }
+
     
+
 
 
 }
