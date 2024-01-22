24aee3d4d92fb0db27161d59dfc1c29ae548ff28
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-22 21:41:11 +0530

    Added MajorityElement code

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,14 @@
+public class MajorityElement {
+    
+    /*
+     * Given an array of size n, return the majority element. The majority element is the element that appears 
+     * more than (n/2) times,
+     * 
+     * Example ar = {8,5,8,1,2,8,8}
+     * O/P = 8
+    */
+
+    
+
+
+}
