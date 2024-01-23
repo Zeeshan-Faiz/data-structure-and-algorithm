a5b4e4ed7f008d4a4ab5dc8c36e8e34304cbc7dd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-23 22:23:34 +0530

    Added SearchElement code

---
+++ 2D-Array/SearchElement.java
@@ -0,0 +1,24 @@
+public class SearchElement {
+    
+    /*
+     * Write a program to seach for an element in mxn matrix. NOTE: Integers in each row of the matrix are sorted
+     * in ascending order from left to right and Integers in each column are sorted in ascending order from top to
+     * bottom of the matrix.
+     * Return true or false depending whether the target element is present in the matrix or not.
+     * 
+     * Example ar = {{1,4,7,11,15},
+     *              {2,5,8,12,19},
+     *              {3,6,9,16,22},
+     *              {10,13,14,17,24},
+     *              {18,21,23,26,30}}       target = 5
+     * 
+     * O/P = true
+    */
+
+    /*
+     * Approach (Brute Force) : Check for target element by traversing all the elements of the matrix.
+     * 
+     * Time Complexity = O(mxn)
+    */
+
+}
