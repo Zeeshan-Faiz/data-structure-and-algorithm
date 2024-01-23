efb16b27a0e06abfbb5a3d176df6e1ceccfc36bf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-23 22:28:56 +0530

    Added SearchElement code

--- 2D-Array/SearchElement.java
+++ 2D-Array/SearchElement.java
@@ -16,9 +16,9 @@ public class SearchElement {
     */
 
     /*
-     * Approach (Brute Force) : Check for target element by traversing all the elements of the matrix.
+     * Approach 1 (Brute Force) : Check for target element by traversing all the elements of the matrix.
      * 
-     * Time Complexity = O(mxn)
+     * Time Complexity : O(mxn)
     */
 
     static boolean searchElement1(int[][] ar, int target){
@@ -32,6 +32,24 @@ public class SearchElement {
         return false;
     }
 
-    
+    /*
+     * Approach 2 (Optimized) : Instead of traversing the whole matrix to check for the target element as we know
+     * that each row and column of the matrix are sorted in ascending order we,ll start searching for the target element
+     * from last element of the 1st row i.e 15.
+     * If the target element is smaller than 15 we'll move one column backward(j--) otherwise we'll move one row forward(i++)
+     * 
+     * Time Complexity : O(m+n)
+    */
+
+    public static void main(String[] args){
+
+        int ar[][] ={{1,4,7,11,15},
+                      {2,5,8,12,19},
+                      {3,6,9,16,22},
+                      {10,13,14,17,24},
+                      {18,21,23,26,30}};
+
+        System.out.println(searchElement1(ar,5));
+    }
 
 }
