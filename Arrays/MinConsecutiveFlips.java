9706086f11a338e554bffb021cf7ac81992a925a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-23 21:44:55 +0530

    Added MinConsecutiveFlips code

---
+++ Arrays/MinConsecutiveFlips.java
@@ -0,0 +1,29 @@
+public class MinConsecutiveFlips {
+ 
+    /*
+     * You are given an array containing only 1 and 0. You have to find the minimum consecutive flips that will
+     * make the whole array either all 0 or all 1. Remeber that flipping happens groupwise if possible.
+     *  
+     *               0 1 2 3 4 5 6 7 8 9 10
+     * Example ar = {1,1,0,0,1,1,0,0,0,1,1}
+     * O/P = 2-3
+     *       6-8
+     * 
+     * Example ar = {1,1,0,0,0,1,0,0,1,1,0,1}
+     * O/P = 2-4
+     *       6-7
+     *       10-10
+    */
+
+    /*
+     * Approach : After looking closely into the given examples and all other possible combinations that our mind
+     * can think of, we can say that which ever element is present at ar[0] (first element), we'll flip the oppostite
+     * of that element.
+     * Example ar = {1,1,0,0,1,1,0,0,0,1,1}
+     * We have 3 groups of 1 and 2 groups of 0 (As group of 0 is minimum it's better if we try to flip 0)
+     * 
+    */
+
+
+
+}
