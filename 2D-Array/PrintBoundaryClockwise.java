dcd343c572863f0e083dc964641e04015fa51596
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-23 22:15:05 +0530

    Added PrintBoundaryClockwise code

--- 2D-Array/PrintBoundaryClockwise.java
+++ 2D-Array/PrintBoundaryClockwise.java
@@ -32,8 +32,27 @@ public class PrintBoundaryClockwise {
         for(i = 1; 1 < ar.length; i++)
             System.out.print(ar[i][j]);
 
+        //print last row
+        i = ar.length-1;
+        for(j = ar.length-2; j >= 0; j--)
+            System.out.print(ar[i][j]);
+
+        //print first column
+        j = 0;
+        for(i = ar.length-2; i > 0; i--)
+            System.out.print(ar[i][j]);
 
 
+    }
+
+    public static void main(String[] args){
+
+        int ar[][] = {{1,2,3,4},
+                      {5,6,7,8},
+                      {9,10,11,12},
+                      {13,14,15,16}};
+
+        printZigZag(ar);
     }
 
 }
