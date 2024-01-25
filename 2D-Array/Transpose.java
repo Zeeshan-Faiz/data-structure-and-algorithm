d0a8ebbf263e68e01e68ce69abfe2897c0c5943b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-25 22:17:12 +0530

    Added Transpose code

---
+++ 2D-Array/Transpose.java
@@ -0,0 +1,27 @@
+public class Transpose {
+    
+    /*
+     * Write a program to find the transpose of nxn matrix.
+     * 
+     * Example mat = {{1,6,11,16,21},
+     *               {2,7,12,17,22},
+     *               {3,8,13,18,23},
+     *               {4,9,14,19,24},
+     *               {5,10,15,20,25}}
+     * 
+     *  O/P = {{1,2,3,4,5},
+     *        {6,7,8,9,10},
+     *        {11,12,13,14,15},
+     *        {16,17,18,19,20},
+     *        {21,22,23,24,25}}
+    */
+
+    /*
+     * Approach (Brute Force) : We can create a new matrix of the same size of the given matrix, and convert all
+     * row elements of the given matrix to column elements to get the transpose. 
+     * 
+     * Time Complexity = O(n^2)
+     * Space Complexity = O(n^2)
+    */
+
+}
