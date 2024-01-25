a025cb5378ed99d1c3bbedf6411d03c83d269804
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-25 22:23:01 +0530

    Added Transpose code

--- 2D-Array/Transpose.java
+++ 2D-Array/Transpose.java
@@ -39,10 +39,24 @@ public class Transpose {
 
         for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat.length; j++){
-                System.out.print(mat[i][j]);
+                System.out.print(mat[i][j] + "  ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args){
+
+        int mat[][] = {{1,6,11,16,21},
+                      {2,7,12,17,22},
+                      {3,8,13,18,23},
+                      {4,9,14,19,24},
+                      {5,10,15,20,25}};
+
+        printMatrix(mat);
+        System.out.println();
+        int[][] res = transpose1(mat);
+        printMatrix(res);
     }
 
 }
