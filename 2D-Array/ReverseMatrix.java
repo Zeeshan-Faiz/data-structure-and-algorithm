5387127249024c95316d6fa64ba82e0776b5e1be
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-25 22:42:28 +0530

    Added ReverseMatrix code

--- 2D-Array/ReverseMatrix.java
+++ 2D-Array/ReverseMatrix.java
@@ -40,5 +40,28 @@ public class ReverseMatrix {
         }
     }
 
+    static void printMatrix(int[][] mat){
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                System.out.print(mat[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+
+        int mat[][] = {{1,6,11,16,21},
+                      {2,7,12,17,22},
+                      {3,8,13,18,23},
+                      {4,9,14,19,24},
+                      {5,10,15,20,25}};
+                    
+        printMatrix(mat);
+        System.out.println();
+
+    }
+
 
 }
