d45ad2f13a4653b606d713266cf599a6e831e41d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-26 19:20:33 +0530

    Added RotateImageBy90 code

---
+++ 2D-Array/RotateImageBy90.java
@@ -0,0 +1,35 @@
+public class RotateImageBy90 {
+    
+    /*
+     * You are given a nxn matrix representing an image, rotate the image by 90 degrees clockwise without
+     * allocating any new matrix.
+     * 
+     * Example mat = {{1,2,3},
+     *               {4,5,6},
+     *               {7,8,9}}
+     * 
+     * O/P = {{7,4,1},
+     *       {8,5,2},
+     *       {9,6,3}}
+    */
+
+    /*
+     * Approach : The direct approach to solve this particular question is to find the transpose of the given matrix
+     * and then reverse it in order to find the rotated matrix.
+     * 
+     * Example mat = {{1,2,3},
+     *               {4,5,6},
+     *               {7,8,9}}
+     * 
+     * transpose(mat) = {{1,4,7},
+     *                  {2,5,8},
+     *                  {3,6,9}}
+     * 
+     * reverse(transpose(mat)) = {{7,4,1},
+     *                            {8,5,2},
+     *                            {9,6,3}}
+    */
+
+
+
+}
