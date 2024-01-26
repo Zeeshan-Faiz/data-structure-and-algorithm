6b3158761f3da37aabce572bd2c477573da0b260
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-26 20:53:54 +0530

    Added AddElementInIndex code

--- LinkedList/AddElementInIndex.java
+++ LinkedList/AddElementInIndex.java
@@ -5,7 +5,7 @@
 class Node3{
     
     int data;
-    Node next;
+    Node3 next;
 
     public Node3(int data) {
         this.data = data;
@@ -15,11 +15,11 @@ class Node3{
 
 class LinkedList3{
 
-    Node head;
+    Node3 head;
 
     void printLinkedList(){
 
-        Node curr = head;
+        Node3 curr = head;
         while(curr != null){
             System.out.print(curr.data + " ");
             curr = curr.next;
@@ -28,24 +28,70 @@ class LinkedList3{
 
     void addAtEnd(int e){
 
-        Node temp = new Node(e);
+        Node3 temp = new Node3(e);
         //if there's no element in the linked list
         if(head == null)
             head = temp;
         
         //if there are elements present in the linked list
         else{
-            Node curr = head;
+            Node3 curr = head;
             while(curr.next != null)
                 curr = curr.next;
             
             curr.next = temp;
         }
     }
+
+    void addFirst(int e){
+
+        Node3 temp = new Node3(e);
+        if(head == null)
+            head = temp;
+        else{
+            temp.next = head;
+            head = temp;
+        }
+    }
+
+    void addAtIndex(int index, int e){
+
+        try{
+            if(index == 0)
+                addFirst(e);
+            else{
+                Node3 temp = new Node3(e);
+                int count = 0;
+                Node3 curr = head;
+                while(count < index-1){
+                    curr = curr.next;
+                    count++;
+                }
+                temp.next = curr.next;
+                curr.next = temp;
+            }
+        }
+        //If given index is greater than the current Linked List
+        catch(NullPointerException ex){
+            throw new IndexOutOfBoundsException();
+        }
+    }
 }
 
 public class AddElementInIndex {
     
+    public static void main(String[] args) {
+        
+        LinkedList3 ll = new LinkedList3();
+        
+        ll.addAtEnd(10);
+        ll.addAtEnd(20);
+        ll.addAtEnd(30);
+        ll.addAtEnd(40);
 
+        ll.printLinkedList();
+
+
+    }
 
 }
