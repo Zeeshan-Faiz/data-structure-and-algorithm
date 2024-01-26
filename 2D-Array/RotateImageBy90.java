fa1c79a4d73add83d7f0c5548f9c4eb2a9fe9541
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-26 19:23:17 +0530

    Added RotateImageBy90 code

--- 2D-Array/RotateImageBy90.java
+++ 2D-Array/RotateImageBy90.java
@@ -56,4 +56,26 @@ public class RotateImageBy90 {
         }
     }
 
+    static void printMatrix(int[][] mat){
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                System.out.print(mat[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+
+        int mat[][] = {{1,2,3},
+                       {4,5,6},
+                       {7,8,9}};
+                    
+        printMatrix(mat);
+        System.out.println();
+
+        rotateMatrix(mat);
+        printMatrix(mat);
+    }
 }
