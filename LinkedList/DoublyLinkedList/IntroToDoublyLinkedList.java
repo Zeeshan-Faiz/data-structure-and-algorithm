b1638e142563d92a382e3c29065f0980be56b955
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-27 11:48:19 +0530

    Added IntroToDoublyLinkedList code

---
+++ LinkedList/DoublyLinkedList/IntroToDoublyLinkedList.java
@@ -0,0 +1,22 @@
+package DoublyLinkedList;
+
+/*
+ * Regarding the disadvantages of a singly linked list:
+ * i)Unidirectional Traversal: In a singly linked list, you can only traverse the list in one direction – from the 
+ * head (beginning) to the tail (end). If you need to traverse backward, you would need to start from the head 
+ * and traverse the list again, which can be inefficient.
+ * ii)Extra Memory: Singly linked lists require less memory compared to doubly linked lists since they have only 
+ * one reference field. The absence of a "previous" pointer reduces the memory overhead.
+ * iii)Complex Deletion: Deleting a node from the middle of a singly linked list requires traversing from the head 
+ * to find the previous node. This operation can be less efficient compared to a doubly linked list where you 
+ * already have a reference to the previous node.
+ * 
+ * A doubly linked list is a type of linked list where each node contains a data element and two pointers or 
+ * references. These pointers are used to link the node to both its previous and next nodes in the sequence. 
+ * In a doubly linked list, each node has a "previous" pointer pointing to the node before it and a "next" 
+ * pointer pointing to the node after it.
+*/
+
+public class IntroToDoublyLinkedList {
+    
+}
