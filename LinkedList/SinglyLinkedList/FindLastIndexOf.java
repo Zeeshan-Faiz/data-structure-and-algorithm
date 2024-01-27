d0503ceb883dd314c6be47f632a8112dd34fdea2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-27 11:37:40 +0530

    Added FindLastIndexOf code

--- LinkedList/SinglyLinkedList/FindLastIndexOf.java
+++ LinkedList/SinglyLinkedList/FindLastIndexOf.java
@@ -4,6 +4,64 @@ package SinglyLinkedList;
  * Write a program to find the last index of a given element in a linked list.
 */
 
+class Node8{
+    
+    int data;
+    Node8 next;
+
+    public Node8(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList8{
+
+    Node8 head;
+
+    void printLinkedList(){
+
+        Node8 curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    void addAtEnd(int e){
+
+        Node8 temp = new Node8(e);
+        //if there's no element in the linked list
+        if(head == null)
+            head = temp;
+        
+        //if there are elements present in the linked list
+        else{
+            Node8 curr = head;
+            while(curr.next != null)
+                curr = curr.next;
+            
+            curr.next = temp;
+        }
+    }
+
+    int lastIndexOf(int e){
+
+        Node8 curr = head;
+        int count = 0;
+        int index = -1;
+        while(curr != null){
+            if(curr.data == e)
+                index = count;
+            
+            curr = curr.next;
+            count++;
+        }
+        return index;
+    }
+}
+
 public class FindLastIndexOf {
     
 
