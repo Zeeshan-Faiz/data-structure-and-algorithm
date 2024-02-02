1fa15a19d5e5fdd40eeee562743efee3612c0a31
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-02 21:45:45 +0530

    Added FindInMountainArray code

--- Arrays/FindInMountainArray.java
+++ Arrays/FindInMountainArray.java
@@ -29,5 +29,66 @@ Explanation: 3 does not exist in the array, so we return -1.
 */
 
 public class FindInMountainArray {
+
+    int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int peak = peakIndex(mountainArr);
+
+        int firstTry = agnosticArray(mountainArr, target,0,peak);
+        if(firstTry != -1)
+            return firstTry;
+        else
+            return agnosticArray(mountainArr, target, peak+1, mountainArr.length()-1);
+
+    }
+
+    static int peakIndex(MountainArray mountainArr){
+
+        int start = 0;
+        int end = mountainArr.length() - 1;
+        int mid = -1;
+
+        while(start < end){
+            
+            mid = (start+end)/2;
+            if(mountainArr.get(mid) > mountainArr.get(mid+1))
+                //we are in descending part
+                end = mid;
+            else
+                //we are in ascending part
+                start = mid + 1;
+        }
+        return start;
+    }
+
+    static int agnosticArray(MountainArray mountainArr, int target, int start, int end){
+
+        start = start;
+        end = end;
+
+        //check whether the given array is Ascending or Descending.
+        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+        
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(mountainArr.get(mid) == target)
+                return mid;
+            
+            if(isAsc){
+                if(mountainArr.get(mid) < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else{
+                if(mountainArr.get(mid) > target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     
 }
