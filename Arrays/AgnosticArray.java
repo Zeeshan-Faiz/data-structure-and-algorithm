647f79a3a3b00e4d316fb3fff77c1e8c521b294e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-02 19:34:50 +0530

    Added AgnosticArray code

--- Arrays/AgnosticArray.java
+++ Arrays/AgnosticArray.java
@@ -12,9 +12,26 @@ public class AgnosticArray {
 
         //check whether the given array is Ascending or Descending.
         boolean isAsc = arr[0] < arr[arr.length-1];
-        if(isAsc){
-
+        
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(arr[mid] == target)
+                return mid;
+            
+            if(isAsc){
+                if(arr[mid] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else{
+                if(arr[mid] > target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
         }
+        return -1;
     }
     
 }
