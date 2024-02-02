7e52ee358b5863796fab27340cc440e34d3f76cd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-02 11:28:22 +0530

    Added FindIndexInIfiniteSortedArray code

---
+++ Arrays/FindIndexInIfiniteSortedArray.java
@@ -0,0 +1,24 @@
+/*
+Given an array containing infinite sorted integers and an element,  write a program to find the position of the 
+element.”
+
+Example 1:
+Input: N = 89, array[] = {9, 11, 17, 26, 37, 52, 89, 111, 129, 144, 198}
+Output: 6
+*/
+
+public class FindIndexInIfiniteSortedArray {
+
+    /*
+     * Take bunch of elements, apply binary search to find the target element, if the element is not found in this batch
+     * take the next bigger batch and continue finding the target element.
+     * We'll try to double the window size after every iteration ot maximise optimisation.
+    */
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+
+        
+    }
+    
+
+}
