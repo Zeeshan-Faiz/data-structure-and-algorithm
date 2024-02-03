61e318b00b0f10702f9927ce30da8b4ac9420d41
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 12:32:25 +0530

    Added SearchInSortedMatrix code

---
+++ 2D-Array/SearchInSortedMatrix.java
@@ -0,0 +1,18 @@
+/*
+Given a 2D matrix of nxn size whose both rows and columns are sorted Find the target element and return it's inddex.
+
+Example arr[][] = {10, 20, 30, 40},
+                  {15, 25, 35, 45},
+                  {28, 29, 37, 49},
+                  {33, 34, 38, 50}
+
+target = 37
+
+O/P = [2,2]
+*/
+
+public class SearchInSortedMatrix {
+
+    
+    
+}
