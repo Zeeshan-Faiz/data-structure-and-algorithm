9109a905a2c446f3ea4ae9590b7c5863f4d23295
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 12:51:24 +0530

    Added BubbleSort code

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+/*
+Given an integer array, sort the array elements using Bubble Sort.
+*/
+
+public class BubbleSort {
+
+    /*
+    Approach : We'll run two for loops one will(i) track the number of passed made to the element and the other for loop (j)
+    will actually swap the numbers in ascending order. We'll also make sure that we don't iterate over the whole array
+    every time as with each pass(i) the last element will be sorted and also we'll put a flag 'swapped' so that for any
+    pass(i) if there's no swapping happened that we can assume that the array elements are now sorted and we can break
+    from the loop.
+
+    Time Complexity = O(N^2)
+    */
+    
+}
