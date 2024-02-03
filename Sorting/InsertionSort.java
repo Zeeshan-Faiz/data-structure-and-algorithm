61bd8e2d01ff497b8a7616dbcc4786911735be44
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 15:27:17 +0530

    Added InsertionSort code

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+/*
+Given an array of integers, sort the array in ascending order using Insertion Sort technique.
+*/
+
+public class InsertionSort {
+
+    /*
+    Approach : We'll be traversing the array from left to right using the index j and after every pass we'll try to
+    insert the jth element in the left hand side of the array and we'll do this step until all array elements are
+    sorted.
+
+    Time Complexity = O(N^2)
+    */
+
+    
+    
+}
