e05a7a8d46e4eafa777fd313baf6edb83c6ed025
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 12:40:34 +0530

    Added SearchInStrictlySorted code

---
+++ 2D-Array/SearchInStrictlySorted.java
@@ -0,0 +1,21 @@
+/*
+You are given a 2D matrix of size nxn, which is sorted strictly. Find the index of the given target element in the matrix.
+
+Example arr = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}}
+
+target = 5
+O/P = [1,1]
+*/
+
+public class SearchInStrictlySorted {
+
+    /*
+    Approach : We'll try to eliminate rows and columns according to the validation we apply and at the end we'll be
+    left with only two rows and finally we'll try to check the target element in these 2 rows only.
+
+    Time Complexity = O(Log N) + O(Log N) ~ O(Log N)
+    */
+    
+}
