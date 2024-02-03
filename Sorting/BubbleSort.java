9a6d0e870e4ce928fcc445ae144596164e057a48
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 12:52:25 +0530

    Added BubbleSort code

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -14,4 +14,27 @@ public class BubbleSort {
     Time Complexity = O(N^2)
     */
     
+    static void bubbleSort(int[] arr) {
+        
+        boolean swapped;
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            
+            for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than the previous item
+                if (arr[j] < arr[j-1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you did not swap for a particular value of i, it means the array is sorted hence stop the program
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
