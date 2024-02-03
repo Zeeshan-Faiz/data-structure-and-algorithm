3e6bba88ca5ecac7d9017b95d4959d7407a3369f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 12:35:20 +0530

    Added SearchInSortedMatrix code

--- 2D-Array/SearchInSortedMatrix.java
+++ 2D-Array/SearchInSortedMatrix.java
@@ -13,6 +13,30 @@ O/P = [2,2]
 
 public class SearchInSortedMatrix {
 
-    
+    /*
+    Approach : We'll try to define a upper and lower bond with respect to column and row and if the tartget element
+    is less than the last element of the 1st row in the matrix; we'll ignore that column and check to the left columns
+    lese we'll check in the below rows.
+    Time Complexity = O(n)
+    */
+
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix[0].length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+
     
 }
