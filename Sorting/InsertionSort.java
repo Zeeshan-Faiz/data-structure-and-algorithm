cba8773e535c4d82243bf6da9825dfec6e1c0913
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-03 15:28:07 +0530

    Added InsertionSort code

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -12,6 +12,20 @@ public class InsertionSort {
     Time Complexity = O(N^2)
     */
 
-    
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } 
+                else {
+                    break;
+                }
+            }
+        }
+    }
+
     
 }
