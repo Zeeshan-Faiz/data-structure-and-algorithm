1ade99c1742f0246ef8860876aa63be4ab2e5e95
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-04 14:06:35 +0530

    Added SetMismatch code

--- Sorting/SetMismatch.java
+++ Sorting/SetMismatch.java
@@ -17,5 +17,33 @@ Output: [1,2]
 */
 
 public class SetMismatch {
+
+    int[] findErrorNums(int[] nums) {
+        
+        //Apply cyclic sort and sort the array elements
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        //find both duplicate & missing numbers
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                return new int[] {nums[index], index+1};
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
     
 }
