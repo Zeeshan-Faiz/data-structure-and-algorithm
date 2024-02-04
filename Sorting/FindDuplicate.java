9b70779c14dfdc9014c54203c605b2a673c1110b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-04 12:38:57 +0530

    Added FindDuplicate code

--- Sorting/FindDuplicate.java
+++ Sorting/FindDuplicate.java
@@ -42,5 +42,10 @@ public class FindDuplicate {
         arr[first] = arr[second];
         arr[second] = temp;
     }
-    
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
 }
