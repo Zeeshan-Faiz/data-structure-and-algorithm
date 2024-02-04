7e2d71de558956f30af0b428ddecf5c043cfd8b9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-04 12:03:13 +0530

    Added FindAllMissingNumbers code

--- Sorting/FindAllMissingNumbers.java
+++ Sorting/FindAllMissingNumbers.java
@@ -44,4 +44,9 @@ public class FindAllMissingNumbers {
         arr[second] = temp;
     }
     
+    public static void main(String[] args) {
+        
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
 }
