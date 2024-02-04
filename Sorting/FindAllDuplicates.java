96c00ff09fb2ae83d8830bd05c7b12860484f524
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-04 12:56:06 +0530

    Added FindAllDuplicates code

--- Sorting/FindAllDuplicates.java
+++ Sorting/FindAllDuplicates.java
@@ -18,6 +18,38 @@ Input: nums = [1]
 Output: []
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindAllDuplicates {
+
+    static List<Integer> findDuplicates(int[] nums) {
+        
+        //Use Cyclic sort and sort the array elements
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        //find duplicate numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans.add(nums[index]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
     
 }
