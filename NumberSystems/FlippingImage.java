89e57df0c7aefbd4a521f275b9b0ad2b34151079
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-08 19:09:23 +0530

    Added FlippingImage code

--- NumberSystems/FlippingImage.java
+++ NumberSystems/FlippingImage.java
@@ -26,7 +26,7 @@ public class FlippingImage{
         for(int[] row : image) {
             // reverse this array
             for (int i = 0; i < (image[0].length + 1) / 2; i++) {
-                // swap
+                // swap and do XOR to invert each element
                 int temp = row[i] ^ 1;
                 row[i] = row[image[0].length - i - 1] ^ 1;
                 row[image[0].length - i - 1] = temp;
