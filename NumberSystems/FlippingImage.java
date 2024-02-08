43a8e76b055493cbf6a9ce82bc80636572cd66e6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-08 19:05:07 +0530

    Added FlippingImage code

--- NumberSystems/FlippingImage.java
+++ NumberSystems/FlippingImage.java
@@ -21,4 +21,18 @@ Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
 
 public class FlippingImage{
 
+    static int[][] flipAndInvertImage(int[][] image) {
+        
+        for(int[] row : image) {
+            // reverse this array
+            for (int i = 0; i < (image[0].length + 1) / 2; i++) {
+                // swap
+                int temp = row[i] ^ 1;
+                row[i] = row[image[0].length - i - 1] ^ 1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+        return image;
+    }
+
 }
