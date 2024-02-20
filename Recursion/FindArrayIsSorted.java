de58144f1064b011df0c554107bed73ef1884a7f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-20 22:38:36 +0530

    Added FindArrayIsSorted code

--- Recursion/FindArrayIsSorted.java
+++ Recursion/FindArrayIsSorted.java
@@ -3,4 +3,12 @@ public class FindArrayIsSorted {
     /*
      * Given an array find whether it's sorted or not using recursion.
     */
+
+    static boolean sorted(int[] arr, int index) {
+        
+        if (index == arr.length - 1)
+            return true;
+
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
 }
