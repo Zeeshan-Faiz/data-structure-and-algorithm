6340647dd09c2e8e841290a1bbd04b89535bd066
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-20 22:41:34 +0530

    Added FindTargetInArray code

--- Recursion/FindTargetInArray.java
+++ Recursion/FindTargetInArray.java
@@ -10,4 +10,9 @@ public class FindTargetInArray {
         }
         return arr[index] == target || find(arr, target, index + 1);
     }
+
+    public static void main(String[] args) {
+        
+        //System.err.println(find(new int[]{2,3,1,4,4,5}, 1, 0));
+    }
 }
