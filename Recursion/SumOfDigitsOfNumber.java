7c78748230db003581fc85c5e5b1ef32fe3a063d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-20 10:32:57 +0530

    Added SumOfDigitsOfNumber code

--- Recursion/SumOfDigitsOfNumber.java
+++ Recursion/SumOfDigitsOfNumber.java
@@ -5,5 +5,12 @@ public class SumOfDigitsOfNumber {
      * Ex num = 1342
      * O/P = 10
     */
+
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sum(n / 10);
+    }
     
 }
