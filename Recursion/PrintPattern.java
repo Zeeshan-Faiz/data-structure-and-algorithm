f145cc6b6270533ee5e453c422de00a22fc4467b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:38:14 +0530

    Added PrintPattern code

--- Recursion/PrintPattern.java
+++ Recursion/PrintPattern.java
@@ -8,5 +8,20 @@ public class PrintPattern {
        * 
     */
 
-    
+    static void triangle(int r, int c) {
+        
+        if (r == 0) 
+            return;
+
+        if (c < r) {
+            System.out.print("*");
+            triangle(r, c+1);
+        } 
+        else {
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
+
+
 }
