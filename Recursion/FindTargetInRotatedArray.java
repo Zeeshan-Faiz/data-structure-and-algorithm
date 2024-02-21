7bc25f658e2cd057dcca94569dd4b9c21f43def7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:31:20 +0530

    Added FindTargetInRotatedArray code

---
+++ Recursion/FindTargetInRotatedArray.java
@@ -0,0 +1,9 @@
+public class FindTargetInRotatedArray {
+
+    /*
+     * Write a program to find the target elemnent in the rotated array using Recursion.
+     * Ex arr = {5,6,7,1,2,3,4}, target = 4
+     * O/P = 6
+    */
+    
+}
