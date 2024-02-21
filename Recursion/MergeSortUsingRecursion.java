46c2b3fea7445fcef1c70ab9fbefc48ea44f64ae
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:46:23 +0530

    Added MergeSortUsingRecursion code

--- Recursion/MergeSortUsingRecursion.java
+++ Recursion/MergeSortUsingRecursion.java
@@ -18,5 +18,40 @@ public class MergeSortUsingRecursion {
 
         return merge(left, right);
     }
+
+    static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete hence we'll need to copy the remaining elements
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+
     
 }
