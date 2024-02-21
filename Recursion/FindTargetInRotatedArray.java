06adb14e6604494968ee952b1b83814af28a7a5c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:32:17 +0530

    Added FindTargetInRotatedArray code

--- Recursion/FindTargetInRotatedArray.java
+++ Recursion/FindTargetInRotatedArray.java
@@ -5,5 +5,28 @@ public class FindTargetInRotatedArray {
      * Ex arr = {5,6,7,1,2,3,4}, target = 4
      * O/P = 6
     */
+
+    static int search(int[] arr, int target, int s, int e) {
+        
+        if (s > e) 
+            return -1;
+
+        int m = s + (e-s) / 2;
+        if (arr[m] == target)
+            return m;
+
+        if (arr[s] <= arr[m]) {
+
+            if (target >= arr[s] && target <= arr[m])
+                return search(arr, target, s, m-1);
+            else 
+                return search(arr, target, m+1, e);
+        }
+
+        if (target >= arr[m] && target <= arr[e])
+            return search(arr, target, m+1, e);
+
+        return search(arr, target, s, m-1);
+    }
     
 }
