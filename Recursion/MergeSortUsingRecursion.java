bd65f5870ac1f65b765e74beab7d71854fa5e5b8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:45:48 +0530

    Added MergeSortUsingRecursion code

--- Recursion/MergeSortUsingRecursion.java
+++ Recursion/MergeSortUsingRecursion.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+
 public class MergeSortUsingRecursion {
 
     /*
      * Write a program to sort the given array using merge sort via Recursion.
     */
+
+    static int[] mergeSort(int[] arr) {
+       
+        if (arr.length == 1) 
+            return arr;
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
     
 }
