22cc94e9e9c7038e5dca6d25661c7fddba25424a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-21 22:40:39 +0530

    Added BubbleSortUsingRecursion code

--- Recursion/BubbleSortUsingRecursion.java
+++ Recursion/BubbleSortUsingRecursion.java
@@ -3,5 +3,24 @@ public class BubbleSortUsingRecursion {
     /*
      * Write a program to sort the given array using bubble sort via Recursion.
     */
+
+    static void bubble(int[] arr, int r, int c) {
+        if (r == 0)
+            return;
+
+        if (c < r) {
+
+            if (arr[c] > arr[c+1]) {
+                // swap
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+
+            bubble(arr, r, c+1);
+        } 
+        else 
+            bubble(arr, r-1, 0);
+    }
     
 }
