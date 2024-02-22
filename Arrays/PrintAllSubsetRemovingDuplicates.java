13b3bd7a2a625b1ba9a63c1c2188e253d39d4542
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:46:49 +0530

    Added PrintAllSubsetRemovingDuplicates code

--- Arrays/PrintAllSubsetRemovingDuplicates.java
+++ Arrays/PrintAllSubsetRemovingDuplicates.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PrintAllSubsetRemovingDuplicates {
 
     /*
@@ -5,6 +9,33 @@ public class PrintAllSubsetRemovingDuplicates {
      * Ex arr[] = {1,2,2}
      * O/P = 
     */
+
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        
+        //sort the given array to bring duplicate elements adjacent to each other
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        
+        //take start and end index to select from which subset we need to add the next array element
+        int start = 0, end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element is same, s = e + 1
+            if (i > 0 && arr[i] == arr[i-1])
+                start = end + 1;
+            
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                //take previous list present in outer list and directly add in the internal list
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
 
     
     
