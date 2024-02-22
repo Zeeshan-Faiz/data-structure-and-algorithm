d473c8591a16c19130932f94366e36f858d11fc9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:30:11 +0530

    Added ReturnSubseqOfString code

--- Recursion/ReturnSubseqOfString.java
+++ Recursion/ReturnSubseqOfString.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ReturnSubseqOfString {
 
     /*
@@ -20,6 +22,33 @@ public class ReturnSubseqOfString {
 
         //don't pick the character
         subseq(ans, str.substring(1));
+    }
+
+    //Approach 2 : Creating a list of String and then returning the same list
+    static ArrayList<String> subseqRet(String ans, String str) {
+        
+        if (str.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+
+        char ch = str.charAt(0);
+
+        //pick a character
+        ArrayList<String> left = subseqRet(ans + ch, str.substring(1));
+
+        //don't pick the character
+        ArrayList<String> right = subseqRet(ans, str.substring(1));
+
+        //merge both the parts of the lists
+        left.addAll(right);
+        return left;
+    }
+
+    public static void main(String[] args) {
+        
+        subseq("", "abc");
     }
 
     
