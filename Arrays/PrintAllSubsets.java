fca84234aacd42f498617b0ea3edf20a041ca91c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:42:58 +0530

    Added PrintAllSubsets code

--- Arrays/PrintAllSubsets.java
+++ Arrays/PrintAllSubsets.java
@@ -6,17 +6,19 @@ public class PrintAllSubsets {
     /*
      * Write a program to print all the subsets of given array iteratively.
      * Ex arr[] = {1,2,3}
-     * O/P = 
+     * O/P = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
     */
 
      static List<List<Integer>> subset(int[] arr) {
 
+        //outer list
         List<List<Integer>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
-        
+
         for (int num : arr) {
             int n = outer.size();
             for (int i = 0; i < n; i++) {
+                //take previous list present in outer list and directly add in the internal list
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
                 outer.add(internal);
@@ -25,4 +27,8 @@ public class PrintAllSubsets {
         return outer;
     }
 
+    public static void main(String[] args) {
+        
+        System.out.println(subset(new int[]{1,2,3}));
+    }
 }
