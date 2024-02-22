788cc72ce59d37eb72692a4214ed8f5bd467bb72
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 10:52:58 +0530

    Added SkipWordInString code

--- Recursion/SkipWordInString.java
+++ Recursion/SkipWordInString.java
@@ -6,6 +6,16 @@ public class SkipWordInString {
      * O/P = "bcdfg"
     */
 
-    
+    static String skipApple(String str) {
+
+        if (str.isEmpty()) 
+            return "";
+        
+        if (str.startsWith("apple")) 
+            return skipApple(str.substring(5));
+        else 
+            return str.charAt(0) + skipApple(str.substring(1));
+        
+    }
     
 }
