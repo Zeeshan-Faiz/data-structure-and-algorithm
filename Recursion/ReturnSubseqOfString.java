47be0f545bec0cb8da1c5b6a8f116997b717068d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:27:46 +0530

    Added ReturnSubseqOfString code

--- Recursion/ReturnSubseqOfString.java
+++ Recursion/ReturnSubseqOfString.java
@@ -6,6 +6,21 @@ public class ReturnSubseqOfString {
      * O/P = ["a","b","c","ab,"bc","ac","abc",""]
     */
     
+    //Approach 1 : Passing the ans in the method argument
+    static void subseq(String ans, String str) {
+        
+        if (str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+
+        //pick a character
+        subseq(ans + ch, str.substring(1));
+
+        //don't pick the character
+        subseq(ans, str.substring(1));
+    }
 
     
 }
