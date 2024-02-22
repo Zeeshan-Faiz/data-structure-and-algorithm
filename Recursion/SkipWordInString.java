56b5f96b4adf70ff0fc2555f402fd50e729f310a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 10:55:00 +0530

    Added SkipWordInString code

--- Recursion/SkipWordInString.java
+++ Recursion/SkipWordInString.java
@@ -1,7 +1,8 @@
 public class SkipWordInString {
 
     /*
-     * Given a string, skip all the occurence of 'apple' fromt it.
+     * Given a string, skip all the occurence of 'apple' fromt it and another function to remove app if there's no apple in the
+     * given string.
      * Ex str = "bcdapplefg"
      * O/P = "bcdfg"
     */
@@ -17,8 +18,20 @@ public class SkipWordInString {
             return str.charAt(0) + skipApple(str.substring(1)); 
     }
 
+    static String skipAppNotApple(String str) {
+
+        if (str.isEmpty()) 
+            return "";
+        
+        if (str.startsWith("app") && !str.startsWith("apple")) 
+            return skipAppNotApple(str.substring(3));
+        else 
+            return str.charAt(0) + skipAppNotApple(str.substring(1));
+    }
+
     public static void main(String[] args) {
         
-        System.out.println(skipApple("bcdapplefg"));
+        //System.out.println(skipApple("bcdapplefg"));
+        System.out.println(skipAppNotApple("bcdapplfg"));
     }
 }
