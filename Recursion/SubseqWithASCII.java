b3afa4a1c26bfe0d59bcbc0076923c0f0a490714
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:34:49 +0530

    Added SubseqWithASCII code

--- Recursion/SubseqWithASCII.java
+++ Recursion/SubseqWithASCII.java
@@ -5,5 +5,23 @@ public class SubseqWithASCII {
      * Ex str = "ab"
      * O/P = 
     */
+
+    //Approach 1 : passing the ans in the method argument
+    static void subseqAscii(String ans, String str) {
+        if (str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+
+        //pick a character
+        subseqAscii(ans + ch, str.substring(1));
+
+        //don't pick the character
+        subseqAscii(ans, str.substring(1));
+
+        //take only ASCII values
+        subseqAscii(ans + (ch+0), str.substring(1));
+    }
     
 }
