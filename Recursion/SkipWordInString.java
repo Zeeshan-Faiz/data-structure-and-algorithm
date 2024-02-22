4e3d23fc28c32fdce78d7eda49c6a74076f75d1e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 10:52:29 +0530

    Added SkipWordInString code

---
+++ Recursion/SkipWordInString.java
@@ -0,0 +1,11 @@
+public class SkipWordInString {
+
+    /*
+     * Given a string, skip all the occurence of 'apple' fromt it.
+     * Ex str = "bcdapplefg"
+     * O/P = "bcdfg"
+    */
+
+    
+    
+}
