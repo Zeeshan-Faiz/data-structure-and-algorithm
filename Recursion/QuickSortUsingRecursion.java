b168e45e801129a3c5435788d1068c1f5d98e704
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 07:35:34 +0530

    Added QuickSortUsingRecursion code

--- Recursion/QuickSortUsingRecursion.java
+++ Recursion/QuickSortUsingRecursion.java
@@ -4,6 +4,34 @@ public class QuickSortUsingRecursion {
      * Write a program to sort the given array using Quick Sort via Recursion.
     */
 
+    static void sort(int[] nums, int low, int hi) {
+        
+        if (low >= hi)
+            return;
+
+        int s = low, e = hi, m = s + (e - s) / 2;
+        int pivot = nums[m];
+
+        while (s <= e) {
+
+            while (nums[s] < pivot) 
+                s++;
     
+            while (nums[e] > pivot) 
+                e--;
+
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        // now the pivot is at correct index, sort two halves now
+        sort(nums, low, e);
+        sort(nums, s, hi);
+    }
     
 }
