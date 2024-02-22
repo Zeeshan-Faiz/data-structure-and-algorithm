5b3411e153bd478ca21f26c584fa5c080ba210bc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 22:41:41 +0530

    Added PrintAllSubsets code

--- Arrays/PrintAllSubsets.java
+++ Arrays/PrintAllSubsets.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintAllSubsets {
     
     /*
@@ -6,5 +9,20 @@ public class PrintAllSubsets {
      * O/P = 
     */
 
-    
+     static List<List<Integer>> subset(int[] arr) {
+
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
 }
