25882d12a43c8d7e0a7d4931172a420b7f5e756e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 10:46:39 +0530

    Added SkipCharacterInString code

--- Recursion/SkipCharacterInString.java
+++ Recursion/SkipCharacterInString.java
@@ -5,5 +5,20 @@ public class SkipCharacterInString {
      * Ex str = "baccad"
      * O/P = "bccd"
     */
+
+    //Approach 1 : Passing the answer string in the function argument
+    static void skip(String ans, String str) {
+        
+        if (str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+
+        if (ch == 'a') 
+            skip(ans, str.substring(1));
+        else 
+            skip(ans + ch, str.substring(1));
+    }
     
 }
