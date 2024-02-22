06584191824d2a7e1b358445bc45c73993f1977d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-22 07:28:50 +0530

    Added InPlaceMergeSortUsingRecursion code

--- Recursion/InPlaceMergeSortUsingRecursion.java
+++ Recursion/InPlaceMergeSortUsingRecursion.java
@@ -3,5 +3,17 @@ public class InPlaceMergeSortUsingRecursion {
     /*
      * Write a program to sort the given array using In Place Merge Sort technique via Recursion.
     */
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1)
+            return;
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
     
 }
