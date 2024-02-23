b29a3ab0644400c48f027eefb07814166b6e1b92
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-23 11:46:34 +0530

    Added CountAllPermutationOfString code

--- Recursion/CountAllPermutationOfString.java
+++ Recursion/CountAllPermutationOfString.java
@@ -5,5 +5,20 @@ public class CountAllPermutationOfString {
      * Ex str = "abc"
      * O/P = 6
     */
+
+    static int permutationsCount(String ans, String str) {
+        
+        if (str.isEmpty()) 
+            return 1;
+        
+        int count = 0;
+        char ch = str.charAt(0);
+        for (int i = 0; i <= ans.length(); i++) {
+            String f = ans.substring(0, i);
+            String s = ans.substring(i, ans.length());
+            count = count + permutationsCount(f + ch + s, str.substring(1));
+        }
+        return count;
+    }
     
 }
