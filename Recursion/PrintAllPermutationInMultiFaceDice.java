571ce08f8efc7d7f3ec8819df7b70f90daec2200
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-23 12:07:03 +0530

    Added PrintAllPermutationInMultiFaceDice code

---
+++ Recursion/PrintAllPermutationInMultiFaceDice.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class PrintAllPermutationInMultiFaceDice {
+
+    /*
+     * Given a multiface dice print all possible combinations that may be possible for a given target number.
+     * Ex = 4
+     * O/P = [1111, 112, 121, 13, 211, 22, 31, 4]
+    */
+    
+    //Approach 1 : Printing the ans directly after the recusrive call ends
+    static void dice(String ans, int target,int face) {
+        
+        if (target == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 1; i <= face; i++) {
+            if(i <= target)
+                dice(ans + i, target - i,face);
+        }
+    }
+
+    // Approach 2 : Add all the ans in an ArrayList and then return the list
+    static ArrayList<String> diceRet(String ans, int target) {
+        
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        ArrayList<String> res = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            if(i <= target)
+                res.addAll(diceRet(ans + i, target - i));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+        //dice("", 4);
+        System.out.println(diceRet("", 4));
+    }
+}
