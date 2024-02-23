5ffcd2bdba54bca62d35a832ebd09e7c57ba7fbe
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-23 12:00:54 +0530

    Added PrintAllPermutationInDice code

---
+++ Recursion/PrintAllPermutationInDice.java
@@ -0,0 +1,9 @@
+public class PrintAllPermutationInDice {
+
+    /*
+     * Given a normal dice print all possible combinations that may be possible for a given target number.
+     * Ex = 4
+     * O/P = 
+    */
+    
+}
