c4d7d7241a83203e3e867d8002550231e6749bde
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-23 11:40:00 +0530

    Added FindAllPermutationsOfString code

--- Recursion/FindAllPermutationsOfString.java
+++ Recursion/FindAllPermutationsOfString.java
@@ -5,4 +5,22 @@ public class FindAllPermutationsOfString {
      * Ex str = "abc"
      * O/P = [abc,bac,cab,bca,acb,cba]
     */
+
+    static void permutations(String ans, String str) {
+        
+        if (str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+
+        //for every call we'll have different positions to put our current character.
+        // given ba and current ch = c; we can have cba OR bca OR bac
+        //this will take care for all the possible places that the current character can be put
+        for (int i = 0; i <= ans.length(); i++) {
+            String f = ans.substring(0, i);
+            String s = ans.substring(i, ans.length());
+            permutations(f + ch + s, str.substring(1));
+        }
+    }
 }
