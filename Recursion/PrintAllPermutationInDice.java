008bb546326c0cf38b4850acce946b4a06fb58b4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-23 12:01:51 +0530

    Added PrintAllPermutationInDice code

--- Recursion/PrintAllPermutationInDice.java
+++ Recursion/PrintAllPermutationInDice.java
@@ -6,4 +6,16 @@ public class PrintAllPermutationInDice {
      * O/P = 
     */
     
+    static void dice(String ans, int target) {
+        
+        if (target == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 1; i <= 6; i++) {
+            if(i <= target)
+                dice(ans + i, target - i);
+        }
+    }
 }
