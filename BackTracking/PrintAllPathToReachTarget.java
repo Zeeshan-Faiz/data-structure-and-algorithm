0c38ce535bc5e7978f1cc850b43f85deb0ddbdbb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:08:41 +0530

    Added PrintAllPathToReachTarget code

--- BackTracking/PrintAllPathToReachTarget.java
+++ BackTracking/PrintAllPathToReachTarget.java
@@ -2,7 +2,9 @@ public class PrintAllPathToReachTarget {
     
     /*
      * You are in a maze, print all possible path by which you'll be able to reach the target destination
-     * keeping in mind you can only go Right(➡️) OR Down(⬇️)
+     * keeping in mind you can only go Right(➡️) OR Down(⬇️).
+     * Suppose from starting at position (3,3) and destination cell as (0,0) we'll have all possible path as 
+     * []
     */
     
     //Approach 1 : printing the ans directly after each recursive call
@@ -19,6 +21,11 @@ public class PrintAllPathToReachTarget {
         //Going Right
         if (c > 1) 
             path(ans + 'R', r, c-1);
+    }
+
+    public static void main(String[] args) {
+        
+        path("", 3, 3);
     }
 
 }
