eee7d13ba89b31fa87f83bd0e0575495ffc18898
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:12:36 +0530

    Added MoveDiagonal code

---
+++ BackTracking/MoveDiagonal.java
@@ -0,0 +1,10 @@
+public class MoveDiagonal {
+
+    /*
+     * You are in a maze, print all possible path by which you'll be able to reach the target destination
+     * keeping in mind you can only go Right(➡️) OR Down(⬇️) and Diagonal(↘️) as well.
+     * Suppose from starting at position (3,3) and destination cell as (0,0) we'll have all possible path as
+     * [] 
+    */
+    
+}
