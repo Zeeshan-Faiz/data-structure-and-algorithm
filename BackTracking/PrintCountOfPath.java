1cd8226a6eea12400317b177e56a3aa6c7d3b59b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:03:57 +0530

    Added PrintCountOfPath code

--- BackTracking/PrintCountOfPath.java
+++ BackTracking/PrintCountOfPath.java
@@ -1,4 +1,4 @@
-public class PrintAllPathToTarget {
+public class PrintCountOfPath {
 
     /*
      * Write a program to print all the possible combinations of path going from point A to point B in a maze.
