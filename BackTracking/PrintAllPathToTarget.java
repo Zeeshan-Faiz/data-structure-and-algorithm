b0be9005be7546aa7c5b73a0296e5cfe0b53c35f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:02:03 +0530

    Added PrintAllPathToTarget code

--- BackTracking/PrintAllPathToTarget.java
+++ BackTracking/PrintAllPathToTarget.java
@@ -4,6 +4,15 @@ public class PrintAllPathToTarget {
      * Write a program to print all the possible combinations of path going from point A to point B in a maze.
     */
 
-    
+    static int count(int r, int c) {
+
+        if (r == 1 || c == 1) 
+            return 1;
+        
+        int left = count(r-1, c);
+        int right = count(r, c-1);
+        
+        return left + right;
+    }
     
 }
