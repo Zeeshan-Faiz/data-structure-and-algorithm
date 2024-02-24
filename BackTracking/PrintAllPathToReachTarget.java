e2322f4f2e651d71b170df23bea01f5a57b7a90c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:06:03 +0530

    Added PrintAllPathToReachTarget code

---
+++ BackTracking/PrintAllPathToReachTarget.java
@@ -0,0 +1,9 @@
+public class PrintAllPathToReachTarget {
+    
+    /*
+     * You are in a maze, print all possible path by which you'll be able to reach the target destination
+     * keeping in mind you can only go Right(➡️) OR Down(⬇️)
+    */
+    
+    
+}
