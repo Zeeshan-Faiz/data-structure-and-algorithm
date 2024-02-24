a531540a3fdaad30ae400dea386473c67f34fa9d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-24 11:34:08 +0530

    Added MoveWithObstacles code

--- BackTracking/MoveWithObstacles.java
+++ BackTracking/MoveWithObstacles.java
@@ -6,7 +6,7 @@ public class MoveWithObstacles {
     * You are in a maze, print all possible path by which you'll be able to reach the target destination, this time the 
     * maze has some obstacle denoted as false to which you can land and need to take some other path otherwise. 
     * keeping in mind you can only go Right(➡️) OR Down(⬇️).
-    * Suppose from starting at position (0,0) and destination cell as (3,3) we'll have all possible path as
+    * Suppose from starting at position (0,0) and destination cell as (2,2) we'll have all possible path as
     * [DDRR, RRDD]
     */
 
