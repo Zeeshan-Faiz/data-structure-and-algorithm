a303059c6daef7dda4c862084215aaa32bf7547e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-25 12:21:24 +0530

    Added SudokuSolver code

--- BackTracking/SudokuSolver.java
+++ BackTracking/SudokuSolver.java
@@ -5,5 +5,50 @@ public class SudokuSolver {
      * column provided no two equal numbers are adjcent, also keep in mind that each (3x3) sub matrix should only
      * contains numbers from 1-9.
     */
+
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+
+        boolean emptyLeft = true;
+
+        // this is how we are replacing the r,c from arguments
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            // if you found some empty element in row, then break
+            if (emptyLeft == false) {
+                break;
+            }
+        }
+
+        if (emptyLeft == true) {
+            return true;
+            // soduko is solved
+        }
+
+        // backtrack
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) {
+                    // found the answer
+                    return true;
+                } else {
+                    // backtrack
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
     
 }
