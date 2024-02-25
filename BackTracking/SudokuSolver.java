f02cb5336f19e7228b0f98775c690fd93c5fd930
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-25 12:21:06 +0530

    Added SudokuSolver code

---
+++ BackTracking/SudokuSolver.java
@@ -0,0 +1,9 @@
+public class SudokuSolver {
+
+    /*
+     * A Sudoku board is a (9x9) matrix, where in we have to put numbers from 1-9 in each row and 1-9 in each
+     * column provided no two equal numbers are adjcent, also keep in mind that each (3x3) sub matrix should only
+     * contains numbers from 1-9.
+    */
+    
+}
