9211829e160c812617f5a7ae58df73ea4edaadda
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-25 12:01:29 +0530

    Added NQueens code

---
+++ BackTracking/NQueens.java
@@ -0,0 +1,8 @@
+public class NQueens {
+
+    /*
+     * You are given n size of a board, print all the possibilities of adding n queens such that no 2 queens
+     * eliminate each other.
+    */
+    
+}
