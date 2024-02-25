5e670d1a4d57ca1d3ac2125f419d609e8c8b6326
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-25 12:03:22 +0530

    Added NQueens code

--- BackTracking/NQueens.java
+++ BackTracking/NQueens.java
@@ -21,12 +21,37 @@ public class NQueens {
             // place the queen if it is safe cell
             if(isSafe(board, row, col)) {
                 board[row][col] = true;
-                count += queens(board, row + 1);
+                count = count + queens(board, row + 1);
                 board[row][col] = false;
             }
         }
-
         return count;
     }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // check vertical row
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if(board[row-i][col-i]) {
+                return false;
+            }
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if(board[row-i][col+i]) {
+                return false;
+            }
+        }
+
+        return tr
     
 }
