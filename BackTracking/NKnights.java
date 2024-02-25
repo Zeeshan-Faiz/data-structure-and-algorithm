f2dfd44bd6d744562b1a8e408d52d253629ea4b9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-25 12:09:48 +0530

    Added NKnights code

---
+++ BackTracking/NKnights.java
@@ -0,0 +1,11 @@
+public class NKnights {
+
+    /*
+     * You are given n size of a board, print all the possibilities of adding n knights such that no 2 knights
+     * eliminate each other.
+     * Ex if n = 4 (size of board) and 4 (number of knights to be placed on board)
+     * 
+    */
+
+    
+}
