2781de1ba36096e4f7e4b8a845fab62171057950
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 15:19:23 +0530

    Added InsertUsingRecursion code

--- LinkedList/SinglyLinkedList/InsertUsingRecursion.java
+++ LinkedList/SinglyLinkedList/InsertUsingRecursion.java
@@ -5,5 +5,18 @@ public class InsertUsingRecursion {
     /*
      * Insert a node in a singly list using recursion.
     */
-    
+
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
 }
