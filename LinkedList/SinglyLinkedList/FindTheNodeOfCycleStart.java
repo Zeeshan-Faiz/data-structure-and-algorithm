92ea0c04e072134dbb953924c55f664caf35a8a8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 16:17:24 +0530

    Added FindTheNodeOfCycleStart code

--- LinkedList/SinglyLinkedList/FindTheNodeOfCycleStart.java
+++ LinkedList/SinglyLinkedList/FindTheNodeOfCycleStart.java
@@ -45,4 +45,13 @@ public class FindTheNodeOfCycleStart {
         }
         return null;
     }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+        val = x;
+        next = null;
+        }
+    }
 }
