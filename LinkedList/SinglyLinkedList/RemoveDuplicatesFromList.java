6c133199e51e197251d5042dfcd11d652776b3b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 15:33:55 +0530

    Added RemoveDuplicatesFromList code

---
+++ LinkedList/SinglyLinkedList/RemoveDuplicatesFromList.java
@@ -0,0 +1,10 @@
+package SinglyLinkedList;
+
+public class RemoveDuplicatesFromList {
+    
+    /*
+    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
+    Return the linked list sorted as well.
+    
+    */
+}
