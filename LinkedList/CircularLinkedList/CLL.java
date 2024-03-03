40fbbcb4533142e9d6f61b6ca4fee2e35633074f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 15:11:03 +0530

    Added CLL code

--- LinkedList/CircularLinkedList/CLL.java
+++ LinkedList/CircularLinkedList/CLL.java
@@ -1,5 +1,14 @@
 package CircularLinkedList;
 
+import org.w3c.dom.Node;
+
 public class CLL {
-    
+
+    private Node head;
+    private Node tail;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
 }
