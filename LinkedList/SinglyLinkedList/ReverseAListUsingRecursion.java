e60f9c15832fc4b5aaa6ea95e585384d0280d0ea
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 19:12:39 +0530

    Added ReverseAListUsingRecursion code

--- LinkedList/SinglyLinkedList/ReverseAListUsingRecursion.java
+++ LinkedList/SinglyLinkedList/ReverseAListUsingRecursion.java
@@ -1,23 +1,25 @@
 package SinglyLinkedList;
 
 public class ReverseAListUsingRecursion {
-    
-    /*
-    Given the head of a singly linked list, reverse the list, and return the reversed list.
-
-    Example 1:
-    Input: head = [1,2,3,4,5]
-    Output: [5,4,3,2,1]
-
-    Example 2:
-    Input: head = [1,2]
-    Output: [2,1]
-    */
 
     /*
-     * Approach : Firstly move forward until you reach the tail of the list, and from there start coming back
+     * Given the head of a singly linked list, reverse the list, and return the
+     * reversed list.
+     * 
+     * Example 1:
+     * Input: head = [1,2,3,4,5]
+     * Output: [5,4,3,2,1]
+     * 
+     * Example 2:
+     * Input: head = [1,2]
+     * Output: [2,1]
+     */
+
+    /*
+     * Approach : Firstly move forward until you reach the tail of the list, and
+     * from there start coming back
      * while changing the direction of the list.
-    */
+     */
 
     private Node head;
     private Node tail;
@@ -38,4 +40,13 @@ public class ReverseAListUsingRecursion {
         tail.next = null;
     }
 
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
