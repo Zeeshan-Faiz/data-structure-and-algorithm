c0bbe923793b5068e7422eeb66e3593f2ee68e88
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 15:55:48 +0530

    Added LinkedListCycle code

--- LinkedList/SinglyLinkedList/LinkedListCycle.java
+++ LinkedList/SinglyLinkedList/LinkedListCycle.java
@@ -31,6 +31,13 @@ public class LinkedListCycle {
         return false;
     }
 
-
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+        val = x;
+        next = null;
+        }
+    }
     
 }
