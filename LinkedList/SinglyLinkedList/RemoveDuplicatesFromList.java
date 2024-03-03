c7adccdd21d0f020efa9570883b3ad774eda71c7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 15:35:39 +0530

    Added RemoveDuplicatesFromList code

--- LinkedList/SinglyLinkedList/RemoveDuplicatesFromList.java
+++ LinkedList/SinglyLinkedList/RemoveDuplicatesFromList.java
@@ -15,4 +15,27 @@ public class RemoveDuplicatesFromList {
     Input: head = [1,1,2,3,3]
     Output: [1,2,3]
     */
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
 }
