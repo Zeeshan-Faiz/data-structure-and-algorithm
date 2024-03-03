7ceec999f733716f7de6540b060e7bac9d693df0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 16:21:34 +0530

    Added MiddleOfLinkedList code

--- LinkedList/SinglyLinkedList/MiddleOfLinkedList.java
+++ LinkedList/SinglyLinkedList/MiddleOfLinkedList.java
@@ -17,4 +17,9 @@ public class MiddleOfLinkedList {
     Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
     */
     
+    //Approach : Let two pointers start from head of the given linkedlist. One of the pointer's speed is 2x times
+    //more than the other pointer such that when the first pointer reaches the end of LL, the other slower pointer
+    //will reach exactly half of the linkedlist, hence return the node of this slow pointer.
+
+    
 }
