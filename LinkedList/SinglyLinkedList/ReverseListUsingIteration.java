32372f564692d090522d3d7d796aa66cd37973ab
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 19:16:27 +0530

    Added ReverseListUsingIteration code

--- LinkedList/SinglyLinkedList/ReverseListUsingIteration.java
+++ LinkedList/SinglyLinkedList/ReverseListUsingIteration.java
@@ -13,5 +13,35 @@ public class ReverseListUsingIteration {
      * Input: head = [1,2]
      * Output: [2,1]
     */
+
+    public ListNode reverseList(ListNode head) {
+        
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
     
 }
