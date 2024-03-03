8317233b340d7739135f01083b101f8774653681
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 16:22:08 +0530

    Added MiddleOfLinkedList code

--- LinkedList/SinglyLinkedList/MiddleOfLinkedList.java
+++ LinkedList/SinglyLinkedList/MiddleOfLinkedList.java
@@ -3,26 +3,31 @@ package SinglyLinkedList;
 public class MiddleOfLinkedList {
 
     /*
-    Given the head of a singly linked list, return the middle node of the linked list.
-    If there are two middle nodes, return the second middle node.
+     * Given the head of a singly linked list, return the middle node of the linked
+     * list.
+     * If there are two middle nodes, return the second middle node.
+     * 
+     * Example 1:
+     * Input: head = [1,2,3,4,5]
+     * Output: [3,4,5]
+     * Explanation: The middle node of the list is node 3.
+     * 
+     * Example 2:
+     * Input: head = [1,2,3,4,5,6]
+     * Output: [4,5,6]
+     * Explanation: Since the list has two middle nodes with values 3 and 4, we
+     * return the second one.
+     */
 
-    Example 1:
-    Input: head = [1,2,3,4,5]
-    Output: [3,4,5]
-    Explanation: The middle node of the list is node 3.
-
-    Example 2:
-    Input: head = [1,2,3,4,5,6]
-    Output: [4,5,6]
-    Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
-    */
-    
-    //Approach : Let two pointers start from head of the given linkedlist. One of the pointer's speed is 2x times
-    //more than the other pointer such that when the first pointer reaches the end of LL, the other slower pointer
-    //will reach exactly half of the linkedlist, hence return the node of this slow pointer.
+    // Approach : Let two pointers start from head of the given linkedlist. One of
+    // the pointer's speed is 2x times
+    // more than the other pointer such that when the first pointer reaches the end
+    // of LL, the other slower pointer
+    // will reach exactly half of the linkedlist, hence return the node of this slow
+    // pointer.
 
     public ListNode middleNode(ListNode head) {
-        
+
         ListNode s = head;
         ListNode f = head;
 
@@ -31,5 +36,15 @@ public class MiddleOfLinkedList {
             f = f.next.next;
         }
         return s;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
     }
 }
