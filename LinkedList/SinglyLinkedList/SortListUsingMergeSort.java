bf271afe9eb1753c1315dae28daf6b9cb02c391d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 18:16:29 +0530

    Added SortListUsingMergeSort code

--- LinkedList/SinglyLinkedList/SortListUsingMergeSort.java
+++ LinkedList/SinglyLinkedList/SortListUsingMergeSort.java
@@ -14,6 +14,7 @@ public class SortListUsingMergeSort {
     Output: [-1,0,3,4,5]
     */
 
+    //Approach : Using merge sort
     ListNode sortList(ListNode head) {
         
         if (head == null || head.next == null) {
@@ -27,5 +28,22 @@ public class SortListUsingMergeSort {
         return merge(left, right);
     }
 
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
 
 }
