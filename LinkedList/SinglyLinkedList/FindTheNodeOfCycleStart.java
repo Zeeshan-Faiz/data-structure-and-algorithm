fdad3bebde47f38f52a0f65af72d581b09c3fea8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-03 16:16:37 +0530

    Added FindTheNodeOfCycleStart code

--- LinkedList/SinglyLinkedList/FindTheNodeOfCycleStart.java
+++ LinkedList/SinglyLinkedList/FindTheNodeOfCycleStart.java
@@ -9,6 +9,19 @@ public class FindTheNodeOfCycleStart {
     Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
     Note that pos is not passed as a parameter.
 
-    
+    Example 1:
+    Input: head = [3,2,0,-4], pos = 1
+    Output: tail connects to node index 1
+    Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+    Example 2:
+    Input: head = [1,2], pos = 0
+    Output: tail connects to node index 0
+    Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+    Example 3:
+    Input: head = [1], pos = -1
+    Output: no cycle
+    Explanation: There is no cycle in the linked list.
     */
 }
