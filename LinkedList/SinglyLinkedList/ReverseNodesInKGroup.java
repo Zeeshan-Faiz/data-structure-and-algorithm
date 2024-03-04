7bad12bf984773531026d0821a0ffa15c109c373
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 22:43:27 +0530

    Added ReverseNodesInKGroup code

--- LinkedList/SinglyLinkedList/ReverseNodesInKGroup.java
+++ LinkedList/SinglyLinkedList/ReverseNodesInKGroup.java
@@ -3,7 +3,7 @@ package SinglyLinkedList;
 public class ReverseNodesInKGroup {
 
     /*
-     * Given the head of a linked list, reverse the nodes of the list k at a time,
+     * (Q25 LeetCode Question (Hard)) Given the head of a linked list, reverse the nodes of the list k at a time,
      * and return the modified list.
      * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes
      * is not a multiple of k then left-out nodes, in the end, should remain as it is.
