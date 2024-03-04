d11a288a0536771981f1f441f9491e67d812e431
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 22:42:56 +0530

    Added ReverseNodesInKGroup code

--- LinkedList/SinglyLinkedList/ReverseNodesInKGroup.java
+++ LinkedList/SinglyLinkedList/ReverseNodesInKGroup.java
@@ -74,4 +74,13 @@ public class ReverseNodesInKGroup {
         return length;
     }
 
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
