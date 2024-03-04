0ff4d9841809770688501c78139c464380a15eab
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 22:54:25 +0530

    Added RotateList code

--- LinkedList/SinglyLinkedList/RotateList.java
+++ LinkedList/SinglyLinkedList/RotateList.java
@@ -13,4 +13,30 @@ public class RotateList {
     Input: head = [0,1,2], k = 4
     Output: [2,0,1]
     */
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k <= 0 || head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode last = head;
+        int length = 1;
+        while (last.next != null) {
+            last = last.next;
+            length++;
+        }
+
+        last.next = head;
+        int rotations = k % length;
+        int skip = length - rotations;
+        ListNode newLast = head;
+        for (int i = 0; i < skip - 1; i++) {
+            newLast = newLast.next;
+        }
+        head = newLast.next;
+        newLast.next = null;
+
+        return head;
+    }
+
 }
