4180a1b74f1498aa9bae43ea99918db1adc73404
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 22:48:26 +0530

    Added ReorderList code

--- LinkedList/SinglyLinkedList/ReorderList.java
+++ LinkedList/SinglyLinkedList/ReorderList.java
@@ -46,6 +46,34 @@ public class ReorderList {
        }
    }
 
-   
+   public ListNode reverseList(ListNode head) {
+    if (head == null) {
+        return head;
+    }
+    ListNode prev = null;
+    ListNode present = head;
+    ListNode next = present.next;
+
+    while (present != null) {
+        present.next = prev;
+        prev = present;
+        present = next;
+        if (next != null) {
+            next = next.next;
+        }
+    }
+    return prev;
+}
+
+public ListNode middleNode(ListNode head) {
+    ListNode s = head;
+    ListNode f = head;
+
+    while (f != null && f.next != null) {
+        s = s.next;
+        f = f.next.next;
+    }
+    return s;
+}
 
 }
