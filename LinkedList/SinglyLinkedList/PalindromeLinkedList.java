894bb87d592f898974a9db6db25269559e76b4a4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 10:46:49 +0530

    Added PalindromeLinkedList code

--- LinkedList/SinglyLinkedList/PalindromeLinkedList.java
+++ LinkedList/SinglyLinkedList/PalindromeLinkedList.java
@@ -1,17 +1,78 @@
 package SinglyLinkedList;
 
 public class PalindromeLinkedList {
-    
+
     /*
-    Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+     * Given the head of a singly linked list, return true if it is a palindrome or
+     * false otherwise.
+     * 
+     * Example 1:
+     * Input: head = [1,2,2,1]
+     * Output: true
+     * 
+     * Example 2:
+     * Input: head = [1,2]
+     * Output: false
+     */
 
-    Example 1:
-    Input: head = [1,2,2,1]
-    Output: true
+    public boolean isPalindrome(ListNode head) {
 
-    Example 2:
-    Input: head = [1,2]
-    Output: false
-    */
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode rereverseHead = headSecond;
 
+        // compare both the halves
+        while (head != null && headSecond != null) {
+            if (head.val != headSecond.val) {
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(rereverseHead);
+
+        return head == null || headSecond == null;
+    }
+
+    // finding the middle node
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+
+    // reversing a list
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
