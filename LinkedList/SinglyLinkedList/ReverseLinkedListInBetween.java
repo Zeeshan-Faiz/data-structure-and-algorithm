42ee56c0a4d0ee6ea333470ad47a4327dda0a276
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 10:40:49 +0530

    Added ReverseLinkedListInBetween code

--- LinkedList/SinglyLinkedList/ReverseLinkedListInBetween.java
+++ LinkedList/SinglyLinkedList/ReverseLinkedListInBetween.java
@@ -1,56 +1,68 @@
 package SinglyLinkedList;
 
 public class ReverseLinkedListInBetween {
-    
+
     /*
-    (Q92 Leetcode Question(Medium))
-    Given the head of a singly linked list and two integers left and right where left <= right, 
-    reverse the nodes of the list from position left to position right, and return the reversed list.
-
-    Example 1:
-    Input: head = [1,2,3,4,5], left = 2, right = 4
-    Output: [1,4,3,2,5]
-
-    Example 2:
-    Input: head = [5], left = 1, right = 1
-    Output: [5]
+     * (Q92 Leetcode Question(Medium))
+     * Given the head of a singly linked list and two integers left and right where
+     * left <= right,
+     * reverse the nodes of the list from position left to position right, and
+     * return the reversed list.
+     
+     * Example 1:
+     * Input: head = [1,2,3,4,5], left = 2, right = 4
+     * Output: [1,4,3,2,5]
+     
+     * Example 2:
+     * Input: head = [5], left = 1, right = 1
+     * Output: [5]
     */
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        
+
         if (left == right) {
-          return head;
-      }
+            return head;
+        }
 
-      // skip the first left-1 nodes
-      ListNode current = head;
-      ListNode prev = null;
-      for (int i = 0; current != null && i < left - 1; i++) {
-          prev = current;
-          current = current.next;
-      }
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
 
-      ListNode last = prev;
-      ListNode newEnd = current;
+        ListNode last = prev;
+        ListNode newEnd = current;
 
-      // reverse between left and right
-      ListNode next = current.next;
-      for (int i = 0; current != null && i < right - left + 1; i++) {
-          current.next = prev;
-          prev = current;
-          current = next;
-          if (next != null) {
-              next = next.next;
-          }
-      }
+        // reverse between left and right
+        ListNode next = current.next;
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
 
-      if (last != null) {
-          last.next = prev;
-      } else {
-          head = prev;
-      }
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
 
-      newEnd.next = current;
-      return head;
-  }
+        newEnd.next = current;
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
