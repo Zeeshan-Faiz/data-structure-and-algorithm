f40068cb3058e02ae101a55af0fd43c6cfc5e639
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 22:54:42 +0530

    Added RotateList code

--- LinkedList/SinglyLinkedList/RotateList.java
+++ LinkedList/SinglyLinkedList/RotateList.java
@@ -39,4 +39,13 @@ public class RotateList {
         return head;
     }
 
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
