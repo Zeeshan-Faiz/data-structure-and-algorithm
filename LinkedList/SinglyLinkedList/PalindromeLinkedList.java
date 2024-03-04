1c94b0e0ea40067182d503cd68768912a66e8f6f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-04 10:47:30 +0530

    Added PalindromeLinkedList code

--- LinkedList/SinglyLinkedList/PalindromeLinkedList.java
+++ LinkedList/SinglyLinkedList/PalindromeLinkedList.java
@@ -5,15 +5,15 @@ public class PalindromeLinkedList {
     /*
      * Given the head of a singly linked list, return true if it is a palindrome or
      * false otherwise.
-     * 
+     
      * Example 1:
      * Input: head = [1,2,2,1]
      * Output: true
-     * 
+     
      * Example 2:
      * Input: head = [1,2]
      * Output: false
-     */
+    */
 
     public boolean isPalindrome(ListNode head) {
 
