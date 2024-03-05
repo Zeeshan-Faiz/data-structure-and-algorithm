271d1bfe25502894628c1d65c23320aae603cb03
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-05 10:41:18 +0530

    Added CustomStack code

--- Stack/CustomStack.java
+++ Stack/CustomStack.java
@@ -1,5 +1,17 @@
 public class CustomStack {
 
-    
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int ptr = -1;
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
     
 }
