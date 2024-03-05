d36a37db28ed1339c3a559aeb951dc654ceb127b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-05 10:46:23 +0530

    Added CustomQueue code

--- Queue/CustomQueue.java
+++ Queue/CustomQueue.java
@@ -1,4 +1,16 @@
 public class CustomQueue {
     
-    
+    private int[] data;
+
+    private static final int DEFAULT_SIZE = 10;
+
+    int end = 0;
+
+    public CustomQueue(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomQueue(int size) {
+        this.data = new int[size];
+    }
 }
