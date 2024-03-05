a0b098bdbb1562b9b4bbd1a35de3d56fb2b2e769
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-05 10:50:17 +0530

    Added CircularQueue code

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,3 +1,17 @@
 public class CircularQueue {
     
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    protected int end = 0;
+    protected int front = 0;
+    private int size = 0;
+
+    public CircularQueue(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
 }
