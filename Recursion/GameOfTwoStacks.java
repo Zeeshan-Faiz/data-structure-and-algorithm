b23bb7ef73a830a042ef3560397d06114399e47e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-06 22:26:43 +0530

    Added GameOfTwoStacks code

--- Recursion/GameOfTwoStacks.java
+++ Recursion/GameOfTwoStacks.java
@@ -1,6 +1,14 @@
 public class GameOfTwoStacks {
 
     /*
+    Alexa has two stacks of non-negative integers, stack and stack where index denotes the top of the stack. Alexa challenges 
+    Nick to play the following game:
+    In each move, Nick can remove one integer from the top of either stack or stack.
+    Nick keeps a running sum of the integers he removes from the two stacks.
+    Nick is disqualified from the game if, at any point, his running sum becomes greater than some integer given at the 
+    beginning of the game. Nick's final score is the total number of integers he has removed from the two stacks.
+    
+    Given a,b and maxSum for games, find the maximum possible score Nick can achieve.
     
     */
     
