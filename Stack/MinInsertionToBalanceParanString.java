600810578ec08a2770c4cbdfb7f6981d48297f3b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-07 22:49:23 +0530

    Added MinInsertionToBalanceParanString code

--- Stack/MinInsertionToBalanceParanString.java
+++ Stack/MinInsertionToBalanceParanString.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class MinInsertionToBalanceParanString {
     
     /*
@@ -31,5 +33,29 @@ public class MinInsertionToBalanceParanString {
     Explanation: Add '(' to match the first '))', Add '))' to match the last '('.
     */
 
+    static int minInsertions(String s) {
+        
+        Stack<Character> stack=new Stack<>();
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(c == '(')
+                stack.push(c);
+            else
+            {
+                if(i+1<s.length()&& s.charAt(i+1)==')')
+                    i++;
+                else 
+                    count++;
+                if(!stack.isEmpty())
+                    stack.pop();
+                else
+                    count++;
+            }
+        }
+        count = count + 2 * stack.size();
+        return count;
+    }
 
 }
