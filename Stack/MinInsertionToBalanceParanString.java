d515b458b570c5f10d719a369292775b3d772f67
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-07 22:50:58 +0530

    Added MinInsertionToBalanceParanString code

--- Stack/MinInsertionToBalanceParanString.java
+++ Stack/MinInsertionToBalanceParanString.java
@@ -35,16 +35,16 @@ public class MinInsertionToBalanceParanString {
 
     static int minInsertions(String s) {
         
-        Stack<Character> stack=new Stack<>();
-        int count=0;
-        for(int i=0;i<s.length();i++)
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(int i = 0; i < s.length(); i++)
         {
             char c = s.charAt(i);
             if(c == '(')
                 stack.push(c);
             else
             {
-                if(i+1<s.length()&& s.charAt(i+1)==')')
+                if(i+1 < s.length() && s.charAt(i + 1) == ')')
                     i++;
                 else 
                     count++;
