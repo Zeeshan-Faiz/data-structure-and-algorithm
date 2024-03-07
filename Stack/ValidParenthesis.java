68422ff584f55fcb67bc6dd5fc588a524745a6b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-07 22:33:10 +0530

    Added ValidParenthesis code

--- Stack/ValidParenthesis.java
+++ Stack/ValidParenthesis.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class ValidParenthesis {
     
     /*
@@ -19,6 +21,37 @@ public class ValidParenthesis {
     Example 3:
     Input: s = "(]"
     Output: false
-
     */
+
+    static public boolean isValid(String s) {
+         
+        Stack<Character> stack = new Stack<>();
+
+    for (char ch : s.toCharArray()) 
+    {
+      if (ch == '(' || ch == '{' || ch == '[') 
+        stack.push(ch);
+         
+    else 
+        {
+            if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                return false;
+                }
+            }
+            if (ch == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                return false;
+                }
+            }
+            if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                return false;
+                }
+            }
+        }
+    }
+    return stack.isEmpty();
+    }
+
 }
