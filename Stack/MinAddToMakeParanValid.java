3d9950c6a33f41f063108c378d04d299533f1b87
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-07 22:42:34 +0530

    Added MinAddToMakeParanValid code

--- Stack/MinAddToMakeParanValid.java
+++ Stack/MinAddToMakeParanValid.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class MinAddToMakeParanValid {
     
     /*
@@ -21,4 +23,21 @@ public class MinAddToMakeParanValid {
     Input: s = "((("
     Output: 3
     */
+
+    static int minAddToMakeValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') 
+            {
+                if (!stack.isEmpty() && stack.peek() == '(') 
+                    stack.pop();
+                else 
+                    stack.push(ch);
+            } 
+            else
+            stack.push(ch);
+        }
+    return stack.size();
+    }
 }
