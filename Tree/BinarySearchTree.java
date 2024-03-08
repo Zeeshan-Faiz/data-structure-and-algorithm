d7dcd1f1f427ebf57b67795477c7c1af111b9d49
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-08 21:54:15 +0530

    Added BinarySearchTree code

--- Tree/BinarySearchTree.java
+++ Tree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 public class BinarySearchTree {
 
+    /*
+    A Binary Search Tree is a tree in which every node has a value that is greater than or equal to the node values in the left sub-tree, 
+    and less than or equal to the node values in the right sub-tree.
+    */
+
     public BinarySearchTree() {
     }
 
