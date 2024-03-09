3ef897a47412343712ef99a08730cd522cf6b96d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-09 15:25:56 +0530

    Added SegmentTree code

--- Tree/SegmentTree.java
+++ Tree/SegmentTree.java
@@ -3,5 +3,25 @@ public class SegmentTree {
     /*
     
     */
+
+    private static class Node {
+        int data;
+        int startInterval;
+        int endInterval;
+        Node left;
+        Node right;
+    
+        public Node (int startInterval, int endInterval) {
+          this.startInterval = startInterval;
+          this.endInterval = endInterval;
+        }
+    }
+    
+    Node root;
+    
+    public SegmentTree(int[] arr) {
+        // create a tree using this array
+        this.root = constructTree(arr, 0, arr.length - 1);
+    }
     
 }
