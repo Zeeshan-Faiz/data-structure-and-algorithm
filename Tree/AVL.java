41d17208f46fc6d6f27137f1cbcb0d48ca5b920f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-09 11:54:44 +0530

    Added AVL code

--- Tree/AVL.java
+++ Tree/AVL.java
@@ -1,5 +1,11 @@
 public class AVL {
 
+    /*
+    The AVL Tree, named after its inventors Adelson-Velsky and Landis, is a self-balancing binary search tree (BST).
+    A self-balancing tree is a binary search tree that balances the height after insertion and deletion according to 
+    some balancing rules.
+    */
+
     
     
 }
