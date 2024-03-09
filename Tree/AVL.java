3e1e92809d8e2d9306066087cc076892c7db1c0e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-09 11:56:36 +0530

    Added AVL code

--- Tree/AVL.java
+++ Tree/AVL.java
@@ -6,6 +6,10 @@ public class AVL {
     some balancing rules.
     */
 
+    public AVL(){
+
+    }
+
 
     public class Node {
         private int value;
@@ -22,6 +26,44 @@ public class AVL {
         }
       }
     
-      private Node root;
+    private Node root;
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        
+        if (node == null) 
+          return -1;
+        
+        return node.height;
+    }
+      
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+    
+    private Node insert(int value, Node node) {
+        
+        if (node == null) {
+          node = new Node(value);
+          return node;
+        }
+    
+        if (value < node.value) {
+          node.left = insert(value, node.left);
+        }
+    
+        if (value > node.value) {
+          node.right = insert(value, node.right);
+        }
+    
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        
+        //check whether the newly aded node has made the tree unbalances
+        return rotate(node);
+    }
+    
     
 }
