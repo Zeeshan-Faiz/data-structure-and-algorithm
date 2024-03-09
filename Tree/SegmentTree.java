64a9637911a884897449a30cef402bb47953b42c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-09 15:26:38 +0530

    Added SegmentTree code

--- Tree/SegmentTree.java
+++ Tree/SegmentTree.java
@@ -1,10 +1,13 @@
 public class SegmentTree {
 
     /*
-    
+    A Segment Tree is a data structure that stores information about array intervals as a tree. 
+    This allows answering range queries over an array efficiently, while still being flexible enough to 
+    allow quick modification of the array.
     */
 
     private static class Node {
+        
         int data;
         int startInterval;
         int endInterval;
