3b97e3d4b40fd1d944bf75128af7fc64ef3ff41e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-09 15:34:48 +0530

    Added SegmentTree code

--- Tree/SegmentTree.java
+++ Tree/SegmentTree.java
@@ -131,4 +131,12 @@ public class SegmentTree {
         return node.data;
     }
 
+    public static void main(String[] args) {
+        
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
+        SegmentTree tree = new SegmentTree(arr);
+        //tree.display();
+
+        System.out.println(tree.query(1, 6));
+    }
 }
