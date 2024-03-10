531789191bee6a11491eb98e5cf0600953a8ee8b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 15:21:05 +0530

    Added LevelOrderTraversal code

--- Tree/AverageOfLevels.java
+++ Tree/AverageOfLevels.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class AverageOfLevels {
     
     /*
@@ -16,5 +23,33 @@ public class AverageOfLevels {
     Output: [3.00000,14.50000,11.00000]
     */
 
-    
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      double averageLevel = 0;
+      for (int i=0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+        averageLevel += currentNode.val;
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+      }
+        averageLevel = averageLevel / levelSize;
+        result.add(averageLevel);
+    }
+    return result;
+    }
+
 }
