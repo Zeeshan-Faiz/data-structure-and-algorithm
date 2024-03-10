6f14fde33494e8782a79bcdb5879d82824aad5de
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 15:02:01 +0530

    Added LevelOrderTraversal code

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class LevelOrderTraversal {
     
     /*
+    (Q102 Leetcode Question(Meduim))
     Given the root of a binary tree, return the level order traversal of its nodes' values. 
     (i.e., from left to right, level by level).
 
@@ -19,5 +27,33 @@ public class LevelOrderTraversal {
     Output: []
     */
 
-    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+         
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+        return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+        int levelSize = queue.size();
+        List<Integer> currentLevel = new ArrayList<>(levelSize);
+        for (int i=0; i < levelSize; i++) {
+            TreeNode currentNode = queue.poll();
+            currentLevel.add(currentNode.val);
+            if (currentNode.left != null) {
+            queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+            queue.offer(currentNode.right);
+            }
+        }
+        result.add(currentLevel);
+        }
+        return result;
+    }
+
 }
