7bdeb3edf769545294242a27a41763280ac6fc6c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 16:10:47 +0530

    Added RightlevelOrderSuccessor code

--- Tree/RightlevelOrderSuccessor.java
+++ Tree/RightlevelOrderSuccessor.java
@@ -2,52 +2,54 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class RightlevelOrderSuccessor {
-    
-    /*
-    Given the root of a Binary tree and a key value find the level order right successor.
 
-    Example 1:
-    Input: root = [3,9,20,15,7], key = 9
-    Output: 20
+    /*
+     * Given the root of a Binary tree and a key value find the level order right successor.
+
+     * Example 1:
+     * Input: root = [3,9,20,15,7], key = 9
+     * Output: 20
     */
 
-     public TreeNode findSuccessor(TreeNode root, int key){
-        
-        if (root == null) 
-        return null;
+    public TreeNode findSuccessor(TreeNode root, int key) {
+
+        if (root == null)
+            return null;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
         while (!queue.isEmpty()) {
 
-                TreeNode currentNode = queue.poll();
-                if (currentNode.left != null) {
+            TreeNode currentNode = queue.poll();
+            if (currentNode.left != null)
                 queue.offer(currentNode.left);
-                }
-                if (currentNode.right != null) {
+
+            if (currentNode.right != null)
                 queue.offer(currentNode.right);
-                }
-                if (currentNode.val == key) {
+
+            //check whether the current node is equal to the given key
+            if (currentNode.val == key)
                 break;
-            }
         }
-        return queue.peek(); 
+        //return the right node of the current node stored in the queue
+        return queue.peek();
     }
 
     public class TreeNode {
-        
+
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
-
 }
