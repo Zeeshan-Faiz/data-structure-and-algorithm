190b40f5bd8519cbe51ca09a4755af71a3ec0ace
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 16:58:23 +0530

    Added LevelOrderTraversalII code

--- Tree/LevelOrderTraversalII.java
+++ Tree/LevelOrderTraversalII.java
@@ -57,4 +57,19 @@ public class LevelOrderTraversalII {
         }
         return result;
     }
+
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
