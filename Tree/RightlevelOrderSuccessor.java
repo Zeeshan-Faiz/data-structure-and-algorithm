eede5a29d295125ccb4290ac409ce46d47eb3823
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 15:49:07 +0530

    Added RightlevelOrderSuccessor code

---
+++ Tree/RightlevelOrderSuccessor.java
@@ -0,0 +1,10 @@
+public class RightlevelOrderSuccessor {
+    
+    /*
+    Given the root of a Binary tree and a key value find the level order right successor.
+
+    Example 1:
+    Input: root = [3,9,20,15,7], key = 9
+    Output: 20
+    */
+}
