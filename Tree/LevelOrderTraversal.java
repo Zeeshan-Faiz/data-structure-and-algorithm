81cc0f545b3bf9e474a0636860eeb7087caca9d4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 15:04:55 +0530

    Added LevelOrderTraversal code

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -31,29 +31,47 @@ public class LevelOrderTraversal {
          
         List<List<Integer>> result = new ArrayList<>();
 
-        if (root == null) {
+        if (root == null) 
         return result;
-        }
 
+        //Use queue to add nodes levelwise
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
         while (!queue.isEmpty()) {
-        int levelSize = queue.size();
-        List<Integer> currentLevel = new ArrayList<>(levelSize);
-        for (int i=0; i < levelSize; i++) {
-            TreeNode currentNode = queue.poll();
-            currentLevel.add(currentNode.val);
-            if (currentNode.left != null) {
-            queue.offer(currentNode.left);
+
+            int levelSize = queue.size();
+            List<Integer> currentLevelResult = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+
+                //take out node from queue and add it in the list
+                TreeNode currentNode = queue.poll();
+                currentLevelResult.add(currentNode.val);
+                
+                //adding the children of current node in the queue
+                if (currentNode.left != null) 
+                queue.offer(currentNode.left);
+                
+                if (currentNode.right != null) 
+                queue.offer(currentNode.right);
             }
-            if (currentNode.right != null) {
-            queue.offer(currentNode.right);
-            }
-        }
-        result.add(currentLevel);
+            result.add(currentLevelResult);
         }
         return result;
     }
 
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
