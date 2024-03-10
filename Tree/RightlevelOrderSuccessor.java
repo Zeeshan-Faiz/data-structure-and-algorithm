5fe9dba35c4686069c81372998216e769213666b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-10 15:49:41 +0530

    Added RightlevelOrderSuccessor code

--- Tree/RightlevelOrderSuccessor.java
+++ Tree/RightlevelOrderSuccessor.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class RightlevelOrderSuccessor {
     
     /*
@@ -7,4 +12,30 @@ public class RightlevelOrderSuccessor {
     Input: root = [3,9,20,15,7], key = 9
     Output: 20
     */
+
+     public TreeNode findSuccessor(TreeNode root, int key){
+    if (root == null) {
+      return null;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+        TreeNode currentNode = queue.poll();
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+        if (currentNode.val = key) {
+          break;
+        }
+    }
+    return queue.peek(); 
+  }
+
+
 }
