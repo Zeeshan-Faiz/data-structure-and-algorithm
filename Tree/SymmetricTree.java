587d7798997138dac923806caa4dda8e6f5123ac
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-11 07:52:10 +0530

    Added SymmetricTree code

--- Tree/SymmetricTree.java
+++ Tree/SymmetricTree.java
@@ -46,4 +46,19 @@ public class SymmetricTree {
         }
         return true;
     }
+
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
