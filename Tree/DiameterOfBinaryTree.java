cff079ea5aae3049d673723061e0b747ae6924b1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-11 10:34:47 +0530

    Added DiameterOfBinaryTree code

--- Tree/DiameterOfBinaryTree.java
+++ Tree/DiameterOfBinaryTree.java
@@ -1,5 +1,3 @@
-import javax.swing.tree.TreeNode;
-
 public class DiameterOfBinaryTree {
     
     /*
@@ -26,9 +24,9 @@ public class DiameterOfBinaryTree {
     }
 
     int height(TreeNode node) {
-        if(node == null) {
+        
+        if(node == null) 
             return 0;
-        }
 
         int leftHeight = height(node.left);
         int rightHeight = height(node.right);
@@ -39,5 +37,18 @@ public class DiameterOfBinaryTree {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 
-
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
