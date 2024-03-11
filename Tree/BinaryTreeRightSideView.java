19c0c5af904e63f82f3bac10fd80f26197000b73
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-11 06:58:36 +0530

    Added BinaryTreeRightSideView code

--- Tree/BinaryTreeRightSideView.java
+++ Tree/BinaryTreeRightSideView.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class BinaryTreeRightSideView {
     
     /*
@@ -17,5 +24,35 @@ public class BinaryTreeRightSideView {
     Output: []
     */
 
-    
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) 
+            return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i=0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+
+                //add last node of each level
+                if (i == levelSize - 1) 
+                result.add(currentNode.val);
+                
+                
+                if (currentNode.left != null) 
+                queue.offer(currentNode.left);
+                
+                if (currentNode.right != null) 
+                queue.offer(currentNode.right);
+                
+            }
+        }
+        return result;
+    }
 }
