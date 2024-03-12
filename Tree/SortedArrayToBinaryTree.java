88820a1c10971d761e2a69968885c66cda834b25
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-12 10:19:38 +0530

    Added SortedArrayToBinaryTree code

--- Tree/SortedArrayToBinaryTree.java
+++ Tree/SortedArrayToBinaryTree.java
@@ -1,5 +1,3 @@
-import javax.swing.tree.TreeNode;
-
 public class SortedArrayToBinaryTree {
 
     /*
@@ -62,4 +60,18 @@ public class SortedArrayToBinaryTree {
         return node;
     }
 
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
