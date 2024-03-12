a5999a218285d2ca7e6328be9710c009d534945a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-12 10:19:56 +0530

    Added SortedArrayToBinaryTree code

--- Tree/SortedArrayToBinaryTree.java
+++ Tree/SortedArrayToBinaryTree.java
@@ -13,7 +13,7 @@ public class SortedArrayToBinaryTree {
      * Input: nums = [1,3]
      * Output: [3,1]
      * Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
-     */
+    */
 
     TreeNode root;
 
