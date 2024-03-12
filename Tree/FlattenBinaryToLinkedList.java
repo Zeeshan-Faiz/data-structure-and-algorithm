73f8690484225ee991e5ff44a48ebef74e004e9a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-12 19:53:21 +0530

    Added FlattenBinaryToLinkedList code

--- Tree/FlattenBinaryToLinkedList.java
+++ Tree/FlattenBinaryToLinkedList.java
@@ -1,3 +1,5 @@
+import javax.swing.tree.TreeNode;
+
 public class FlattenBinaryToLinkedList {
     
     /*
@@ -12,6 +14,23 @@ public class FlattenBinaryToLinkedList {
     Output: [1,null,2,null,3,null,4,null,5,null,6]
     */
 
+    public void flatten(TreeNode root) {
+        
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) {
+                    TreeNode temp = current.left;
+                    
+                    while(temp.right != null) {
+                        temp = temp.right;
+                }
 
+                temp.right = current.right;
+                current.right = current.left;
+                current.left= null;
+            }
+        current = current.right;
+        }
+    }
 
 }
