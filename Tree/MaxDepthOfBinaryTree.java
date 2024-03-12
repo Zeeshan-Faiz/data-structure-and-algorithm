e6472f662f7d3d216afbd6616a0be5114e053f31
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-12 09:52:21 +0530

    Added MaxDepthOfBinaryTree code

--- Tree/MaxDepthOfBinaryTree.java
+++ Tree/MaxDepthOfBinaryTree.java
@@ -6,6 +6,13 @@ public class MaxDepthOfBinaryTree {
     node down to the farthest leaf node.
 
     Example 1:
-    
+    Input: root = [3,9,20,null,null,15,7]
+    Output: 3
+
+    Example 2:
+    Input: root = [1,null,2]
+    Output: 2
     */
+
+    
 }
