f5e8fe919a757dd4b337f79f3e234fa91319956e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-13 09:52:56 +0530

    Added ValidateBinarySearchTree code

--- Tree/ValidateBinarySearchTree.java
+++ Tree/ValidateBinarySearchTree.java
@@ -19,7 +19,27 @@ public class ValidateBinarySearchTree {
     Explanation: The root node's value is 5 but its right child's value is 4.
     */
 
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
 
+    public boolean helper(TreeNode node, Integer low, Integer high) {
+        
+        if (node == null) 
+            return true;
+        
+
+        if (low != null && node.val <= low) 
+            return false;
+        
+        if(high != null && node.val >= high) 
+            return false;
+        
+        boolean leftTree = helper(node.left, low, node.val);
+        boolean rightTree = helper(node.right, node.val, high);
+
+        return leftTree && rightTree;
+    }
 
     public class TreeNode {
         
