036ec650ab28faacbfa1a74f42912fe335cf8528
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-13 09:52:17 +0530

    Added ValidateBinarySearchTree code

--- Tree/ValidateBinarySearchTree.java
+++ Tree/ValidateBinarySearchTree.java
@@ -10,6 +10,14 @@ public class ValidateBinarySearchTree {
         Both the left and right subtrees must also be binary search trees.
 
     Example 1:
-    
+    Input: root = [2,1,3]
+    Output: true
+
+    Example 2:
+    nput: root = [5,1,4,null,null,3,6]
+    Output: false
+    Explanation: The root node's value is 5 but its right child's value is 4.
     */
+
+    
 }
