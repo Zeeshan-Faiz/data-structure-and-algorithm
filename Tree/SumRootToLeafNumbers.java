dad3bf63b9b4e677e9699153a34677cc3b8ddb28
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-14 19:35:23 +0530

    Added SumRootToLeafNumbers code

---
+++ Tree/SumRootToLeafNumbers.java
@@ -0,0 +1,12 @@
+public class SumRootToLeafNumbers {
+    
+    /*
+    You are given the root of a binary tree containing digits from 0 to 9 only.
+    Each root-to-leaf path in the tree represents a number.
+        For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+    Return the total sum of all root-to-leaf numbers.
+
+    Example 1:
+    
+    */
+}
