75e055d3cf7f97a3eb0e793203cea7cb87069ad4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-14 19:36:04 +0530

    Added SumRootToLeafNumbers code

--- Tree/SumRootToLeafNumbers.java
+++ Tree/SumRootToLeafNumbers.java
@@ -24,5 +24,18 @@ public class SumRootToLeafNumbers {
     Therefore, sum = 495 + 491 + 40 = 1026.
     */
 
-    
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
