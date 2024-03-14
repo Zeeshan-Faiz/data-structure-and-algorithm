95a0f1e64de103ce9861bbc595197d09c929af1b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-14 19:59:15 +0530

    Added BinaryTreeMaximumPathSum code

--- Tree/BinaryTreeMaximumPathSum.java
+++ Tree/BinaryTreeMaximumPathSum.java
@@ -15,6 +15,11 @@ public class BinaryTreeMaximumPathSum {
 
     Example 2:
 
+    Input: root = [-10,9,20,null,null,15,7]
+    Output: 42
+    Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
     */
+
+    
     
 }
