b19edd8e69154546c9d3519aaa84e023dd75cf91
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-14 19:33:14 +0530

    Added PathSum code

--- Tree/PathSum.java
+++ Tree/PathSum.java
@@ -18,6 +18,18 @@ public class PathSum {
     There is no root-to-leaf path with sum = 5.
     */
 
-    
-
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
