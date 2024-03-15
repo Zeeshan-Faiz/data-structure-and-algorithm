aa6a6b8dc8f53548885468819165c5f302ef7fff
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:26:40 +0530

    Added GivenPathIsPresentOrNot code

--- Tree/GivenPathIsPresentOrNot.java
+++ Tree/GivenPathIsPresentOrNot.java
@@ -1,17 +1,18 @@
 import org.w3c.dom.Node;
 
 public class GivenPathIsPresentOrNot {
-    
-    /*
-    Given the root of a Binary tree and an array containing the path values. Find whether the given
-    path is present in the tree or not.
-    */
 
-     boolean findPath(TreeNode node, int[] arr) {
-        
-        if (node == null) 
+    /*
+     * Given the root of a Binary tree and an array containing the path values. Find
+     * whether the given
+     * path is present in the tree or not.
+     */
+
+    boolean findPath(TreeNode node, int[] arr) {
+
+        if (node == null)
             return arr.length == 0;
-    
+
         return helper(node, arr, 0);
     }
 
@@ -20,23 +21,30 @@ public class GivenPathIsPresentOrNot {
         if(node == null) 
           return false;
         
-        if(index >= arr.length || node.val != arr[index]) {
+        //if index is out of bound or current node value is not equal to array value, return false.
+        if(index >= arr.length || node.val != arr[index]) 
           return false;
-        }
-        if(node.left == null && node.right == null && index = arr.length - 1) {
+        
+        //if you are at the leaf node and at the last index of the array, return true
+        if(node.left == null && node.right == null && index == arr.length - 1) 
           return true;
-        } 
+        
         return helper(node.left, arr, index + 1) || helper(node.right, arr, index + 1);
       }
 
     public class TreeNode {
-        
+
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
