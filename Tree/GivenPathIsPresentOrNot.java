6533aed79e99264979435b4ba489a6e4b2486efc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:23:44 +0530

    Added GivenPathIsPresentOrNot code

---
+++ Tree/GivenPathIsPresentOrNot.java
@@ -0,0 +1,9 @@
+public class GivenPathIsPresentOrNot {
+    
+    /*
+    Given the root of a Binary tree and an array containing the path values. Find whether the given
+    path is present in the tree or not.
+    */
+
+
+}
