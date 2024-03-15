079be0aba09476427f7f50c07fdab82349a7d391
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:25:11 +0530

    Added GivenPathIsPresentOrNot code

--- Tree/GivenPathIsPresentOrNot.java
+++ Tree/GivenPathIsPresentOrNot.java
@@ -15,6 +15,20 @@ public class GivenPathIsPresentOrNot {
         return helper(node, arr, 0);
     }
 
+    boolean helper(TreeNode node, int[] arr, int index) {
+        
+        if(node == null) 
+          return false;
+        
+        if(index >= arr.length || node.val != arr[index]) {
+          return false;
+        }
+        if(node.left == null && node.right == null && index = arr.length - 1) {
+          return true;
+        } 
+        return helper(node.left, arr, index + 1) || helper(node.right, arr, index + 1);
+      }
+
     public class TreeNode {
         
         int val;
