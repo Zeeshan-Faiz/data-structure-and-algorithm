b1c2349fd221203d91e845e44c81583580ab362a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:36:39 +0530

    Added ImplementDFSUsingStack code

--- Tree/ImplementDFSUsingStack.java
+++ Tree/ImplementDFSUsingStack.java
@@ -1,8 +1,16 @@
+import org.w3c.dom.Node;
+
 public class ImplementDFSUsingStack {
     
     /*
     Given the root of a binary tree, traverse the tree in DFS(preorder) and print each nodes.
     */
 
+    public void dfsStack(Node node) {
+    if(node == null) {
+      return;
+    }
+
     
+
 }
