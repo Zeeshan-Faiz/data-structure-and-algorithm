2e7610b16ef94564c8add56f72201d1f193ba882
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:28:22 +0530

    Added CountAllPossiblePaths code

---
+++ Tree/CountAllPossiblePaths.java
@@ -0,0 +1,9 @@
+public class CountAllPossiblePaths {
+    
+    /*
+    Given the root of a binary tree and a targetSum, find and count all the possible paths that is equal to
+    the given targetSum.
+    */
+
+    
+}
