0a48726ce2ec29b6ff22022c413c0361e9eb5b4e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:23:56 +0530

    Added GivenPathIsPresentOrNot code

--- Tree/GivenPathIsPresentOrNot.java
+++ Tree/GivenPathIsPresentOrNot.java
@@ -5,5 +5,18 @@ public class GivenPathIsPresentOrNot {
     path is present in the tree or not.
     */
 
-
+    public class TreeNode {
+        
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
