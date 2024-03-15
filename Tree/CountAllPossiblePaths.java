7811ae9d59eb7de8fdc45e0dba6b546f63113a51
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-15 16:30:33 +0530

    Added CountAllPossiblePaths code

--- Tree/CountAllPossiblePaths.java
+++ Tree/CountAllPossiblePaths.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+
 public class CountAllPossiblePaths {
     
     /*
     Given the root of a binary tree and a targetSum, find and count all the possible paths that is equal to
     the given targetSum.
     */
+
+    public int countPaths(TreeNode node, int sum) {
+        List<Integer> path = new ArrayList<>();
+        return helper(node, sum, path);
+    }
 
     public class TreeNode {
         
