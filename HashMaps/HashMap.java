e6fb827b46c0bc70d48b6a29d5a271d3ec4e012a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-16 15:44:09 +0530

    Added HashMap code

--- HashMaps/HashMap.java
+++ HashMaps/HashMap.java
@@ -2,17 +2,27 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class HashMap<K, V> {
+
     ArrayList<LinkedList<Entity>> list;
 
     private int size = 0;
-
     private float lf = 0.5f;
 
-  public HashMapFinal() {
-    list = new ArrayList<>();
-    for(int i=0; i < 10; i++) {
-      list.add(new LinkedList<>());
+    public HashMap() {
+        list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new LinkedList<>());
+        }
     }
-  }
+
+    private class Entity {
+        K key;
+        V value;
+
+        public Entity(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
 }
