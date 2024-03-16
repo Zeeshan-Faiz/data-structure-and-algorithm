4c9239ca415385782b21e7d39bd53df57b7ccc47
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-16 12:57:20 +0530

    Added Heap

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -16,19 +16,38 @@ public class Heap<T extends Comparable<T>> {
         list.set(second, temp);
     }
 
-    //find parent of any element
+    // find parent of any element
     private int parent(int index) {
         return (index - 1) / 2;
     }
 
-    //find left element of the given index
+    // find left element of the given index
     private int left(int index) {
         return index * 2 + 1;
     }
 
-    //find right element of the given index
+    // find right element of the given index
     private int right(int index) {
         return index * 2 + 2;
+    }
+
+    //inserting an element at the last index and then place it in the correct order according to it's value
+    public void insert(T value) {
+        list.add(value);
+        upheap(list.size() - 1);
+    }
+
+    private void upheap(int index) {
+        
+        //return when you reach the starting index/top of the tree
+        if (index == 0) 
+            return;
+        
+        int p = parent(index);
+        if (list.get(index).compareTo(list.get(p)) < 0) {
+            swap(index, p);
+            upheap(p);
+        }
     }
 
 }
