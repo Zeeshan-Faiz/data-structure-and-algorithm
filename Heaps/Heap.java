d43cf66ac3cdc5c1bd9fa2b1d0fd284492d9cff6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-16 12:56:06 +0530

    Added Heap

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -1,12 +1,34 @@
 import java.util.ArrayList;
 
+//minHeap
 public class Heap<T extends Comparable<T>> {
 
-  private ArrayList<T> list;
+    private ArrayList<T> list;
 
-  public Heap() {
-    list = new ArrayList<>();
-  }
-    
+    public Heap() {
+        list = new ArrayList<>();
+    }
+
+    // function to swap two elements
+    private void swap(int first, int second) {
+        T temp = list.get(first);
+        list.set(first, list.get(second));
+        list.set(second, temp);
+    }
+
+    //find parent of any element
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    //find left element of the given index
+    private int left(int index) {
+        return index * 2 + 1;
+    }
+
+    //find right element of the given index
+    private int right(int index) {
+        return index * 2 + 2;
+    }
 
 }
