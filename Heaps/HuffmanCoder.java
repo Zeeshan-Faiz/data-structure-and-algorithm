b5bba888fa843b89818fe97ef85c5b0a857bdc36
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 15:58:36 +0530

    Added HuffmanCoder code

--- Heaps/HuffmanCoder.java
+++ Heaps/HuffmanCoder.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class HuffmanCoder {
 
     /*
@@ -9,7 +13,7 @@ public class HuffmanCoder {
     HashMap<String, Character> decoder;
 
     private class Node implements Comparable<Node> {
-        
+
         Character data;
         int cost; // frequency
         Node left;
@@ -22,11 +26,58 @@ public class HuffmanCoder {
             this.right = null;
         }
 
-        //we'll compare any two nodes with respect to their cose(i.e frequency)
+        // we'll compare any two nodes with respect to their cose(i.e frequency)
         @Override
         public int compareTo(Node other) {
             return this.cost - other.cost;
         }
+    }
+
+    public HuffmanCoder(String feeder) throws Exception {
+
+        HashMap<Character, Integer> fmap = new HashMap<>();
+
+        // find the frequency of each character in the given string and add in the
+        // frequency map
+        for (int i = 0; i < feeder.length(); i++) {
+
+            char cc = feeder.charAt(i);
+            if (fmap.containsKey(cc)) {
+                int ov = fmap.get(cc);
+                ov += 1;
+                fmap.put(cc, ov);
+            } else
+                fmap.put(cc, 1);
+        }
+
+        Heap<Node> minHeap = new Heap<>();
+        // by using Set we'll be able to get the set of the frequency map one at a time
+        Set<Map.Entry<Character, Integer>> entrySet = ((Map) fmap).entrySet();
+
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            Node node = new Node(entry.getKey(), entry.getValue());
+            minHeap.insert(node);
+        }
+
+        while (((Set<Entry<Character, Integer>>) minHeap).size() != 1) {
+            
+            //take two minimum nodes, combine them and add back in the heap
+            Node first = minHeap.remove();
+            Node second = minHeap.remove();
+
+            Node newNode = new Node('\0', first.cost + second.cost);
+            newNode.left = first;
+            newNode.right = second;
+
+            minHeap.insert(newNode);
+        }
+
+        Node ft = minHeap.remove();
+
+        this.encoder = new HashMap<>();
+        this.decoder = new HashMap<>();
+
+        this.initEncoderDecoder(ft, "");
     }
 
 }
