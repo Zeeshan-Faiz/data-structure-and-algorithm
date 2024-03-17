a929f29fbea710cb502a28a4bae60e47a24a2c9a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 15:54:58 +0530

    Added HuffmanCoder code

--- Heaps/HuffmanCoder.java
+++ Heaps/HuffmanCoder.java
@@ -1,7 +1,32 @@
 public class HuffmanCoder {
 
     /*
-    You are given a string in java, use it Huffman Encoder to compress it and decode it as well.
-    */
-    
+     * You are given a string in java, use it Huffman Encoder to compress it and
+     * decode it as well.
+     */
+
+    HashMap<Character, String> encoder;
+    HashMap<String, Character> decoder;
+
+    private class Node implements Comparable<Node> {
+        
+        Character data;
+        int cost; // frequency
+        Node left;
+        Node right;
+
+        public Node(Character data, int cost) {
+            this.data = data;
+            this.cost = cost;
+            this.left = null;
+            this.right = null;
+        }
+
+        //we'll compare any two nodes with respect to their cose(i.e frequency)
+        @Override
+        public int compareTo(Node other) {
+            return this.cost - other.cost;
+        }
+    }
+
 }
