d78192abfd7fd2907f751ed4e6fa86205f022986
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 12:48:09 +0530

    Added CountSort code

--- Sorting/CountSort.java
+++ Sorting/CountSort.java
@@ -2,7 +2,7 @@ public class CountSort {
 
     /*
      * Given an array, sort the array using CountSort.
-     */
+    */
 
     public static void countSort(int[] array) {
 
@@ -24,11 +24,13 @@ public class CountSort {
             countArray[num]++;
         }
 
+        //now update the original array according to frequencyArray
         int index = 0;
         for (int i = 0; i <= max; i++) {
             while (countArray[i] > 0) {
                 array[index] = i;
                 index++;
+                //reduce the frequency of that index after each addition to the original array
                 countArray[i]--;
             }
         }
