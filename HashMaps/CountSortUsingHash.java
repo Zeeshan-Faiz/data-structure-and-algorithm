2cd71b6644060083abdf5268898fd8a42f4ddf91
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 12:53:26 +0530

    Added CountSortUsingHash code

--- HashMaps/CountSortUsingHash.java
+++ HashMaps/CountSortUsingHash.java
@@ -1,6 +1,33 @@
+import java.util.Arrays;
+import java.util.Map;
+
 public class CountSortUsingHash {
-    
+
     /*
-    
+    Given an array, sort the array using CountSort.
     */
+
+    public static void countSortHash(int[] arr) {
+        
+        if (arr == null || arr.length <= 1) 
+            return;
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+        //add the frequency of each element in the given array to the map as key & value
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        int index = 0;
+        for (int i = min; i <= max; i++) {
+            int count = countMap.getOrDefault(i, 0);
+            for (int j = 0; j < count; j++) {
+                arr[index] = i;
+                index++;
+            }
+        }
+    }
 }
