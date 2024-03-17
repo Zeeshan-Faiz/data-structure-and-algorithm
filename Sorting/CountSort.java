c8d1e5d04d198159f0724cf78f44b865f2065b6c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 12:44:54 +0530

    Added CountSort code

--- Sorting/CountSort.java
+++ Sorting/CountSort.java
@@ -1,6 +1,28 @@
 public class CountSort {
-    
+
     /*
-    Given an array, sort the array using CountSort.
+     * Given an array, sort the array using CountSort.
     */
+
+    public static void countSort(int[] array) {
+        
+        if(array == null || array.length <= 1) {
+          return;
+        }
+    
+        //find the max element from the array
+        int max = array[0];
+        for(int num : array) {
+          if(num > max) 
+            max = num;
+        }
+    
+        //create a frequencyArray to track the numbers in the original array
+        int[] countArray = new int[max + 1];
+    
+        for(int num : array) {
+          countArray[num]++;
+        }
+        
+    }
 }
