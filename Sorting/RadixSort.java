ceb9d2217fcb55c62d9c39b2ea5ff9fab90acfd8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 14:36:53 +0530

    Added RadixSort code

--- Sorting/RadixSort.java
+++ Sorting/RadixSort.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 public class RadixSort {
     
     /*
     Given an array, sort the array using Radix Sort.
     */
 
-    
+    public static void radixSort(int[] arr) {
+    int max = Arrays.stream(arr).max().getAsInt();
+
+    // do count sort for every digit place
+    for(int exp = 1; max/exp > 0; exp *= 10) {
+      countSort(arr, exp);
+    }
+  }
 }
