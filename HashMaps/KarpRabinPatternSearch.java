73c7806e7665745a0747852fe8f7c60604911f78
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-17 11:29:06 +0530

    Added KarpRabinPatternSearch code

--- HashMaps/KarpRabinPatternSearch.java
+++ HashMaps/KarpRabinPatternSearch.java
@@ -1,6 +1,26 @@
 public class KarpRabinPatternSearch {
-    
+
     /*
-    Given two strings text and a pattern, find whether the pattern exists in the given text or not.
+     * Given two strings text and a pattern, find whether the pattern exists in the given text or not.
     */
+
+    private final int PRIME = 101;
+
+    //calculate the hash value of given string
+    private double calculateHash(String str) {
+        double hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash += str.charAt(i) * Math.pow(PRIME, i);
+        }
+        return hash;
+    }
+
+    //calulate the hash value by removing first character from previous hash and then adding a new character
+    //from text and updating the hash
+    private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
+        double newHash = (prevHash - oldChar) / PRIME;
+        newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
+        return newHash;
+    }
+
 }
