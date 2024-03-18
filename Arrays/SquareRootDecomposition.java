afd480f58aeac3c7f8ace917beb4771b2a0e0256
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-18 20:06:50 +0530

    Added SquareRootDecomposition code

--- Arrays/SquareRootDecomposition.java
+++ Arrays/SquareRootDecomposition.java
@@ -2,12 +2,11 @@ public class SquareRootDecomposition {
 
     /*
      * Suppose we are given an array of integers and you are asked to find either
-     * the sum,difference,average of
-     * elements from the index range idx1 to idx2.
-     * 
+     * the sum,difference,average of elements from the index range idx1 to idx2.
+
      * Example arr = {1,3,5,2,7,6,1,4,8}
      * sum(2,7) O/P = 25
-     */
+    */
 
     public static void bulidBlocks(int[] arr) {
 
