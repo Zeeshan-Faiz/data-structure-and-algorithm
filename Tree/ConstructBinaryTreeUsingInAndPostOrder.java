66bb582098f056bcf789368c183b4aa85aafee75
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-18 22:17:47 +0530

    Added ConstructBinaryTreeUsingInAndPostOrder code

--- Tree/ConstructBinaryTreeUsingInAndPostOrder.java
+++ Tree/ConstructBinaryTreeUsingInAndPostOrder.java
@@ -1,5 +1,3 @@
-import javax.swing.tree.TreeNode;
-
 public class ConstructBinaryTreeUsingInAndPostOrder {
 
     /*
@@ -19,10 +17,10 @@ public class ConstructBinaryTreeUsingInAndPostOrder {
     public TreeNode buildTree(int[] preOrder, int[] inOrder) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        //add all the inorder elements in the hashmap along with it's index
         for (int i = 0; i < inOrder.length; i++) {
             map.put(inOrder[i], i);
         }
-
         int[] index = { 0 };
 
         return helper(preOrder, inOrder, 0, preOrder.length - 1, map, index);
@@ -30,10 +28,10 @@ public class ConstructBinaryTreeUsingInAndPostOrder {
 
     public TreeNode helper(int[] preOrder, int[] inOrder, int left, int right, HashMap<Integer, Integer> map,
             int[] index) {
-        if (left > right) {
+        
+        if (left > right) 
             return null;
-        }
-
+        
         int current = preOrder[index[0]];
         index[0]++;
 
@@ -43,11 +41,30 @@ public class ConstructBinaryTreeUsingInAndPostOrder {
             return node;
         }
 
+        //use hashmap to get the element from inorder along with it index
         int inOrderIndex = map.get(current);
 
         node.left = helper(preOrder, inOrder, left, inOrderIndex - 1, map, index);
         node.right = helper(preOrder, inOrder, inOrderIndex + 1, right, map, index);
 
         return node;
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
