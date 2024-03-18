465b93b1f2dd8a2f88da3f2b6378bf07cc454785
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-18 22:15:26 +0530

    Added ConstructBinaryTreeUsingInAndPostOrder code

--- Tree/ConstructBinaryTreeUsingInAndPostOrder.java
+++ Tree/ConstructBinaryTreeUsingInAndPostOrder.java
@@ -1,6 +1,11 @@
 public class ConstructBinaryTreeUsingInAndPostOrder {
  
     /*
+    Given two integer arrays preorder and inorder where preorder is the preorder traversal of a 
+    binary tree and inorder is the inorder traversal of the same tree, construct and return the 
+    binary tree.
+
+    Example 1:
     
     */
 }
