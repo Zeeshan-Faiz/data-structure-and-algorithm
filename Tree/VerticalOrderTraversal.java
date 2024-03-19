77d80d82fcabfe2dc2f649d1e247634a49ca7247
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-19 19:49:16 +0530

    Added VerticalOrderTraversal code

--- Tree/VerticalOrderTraversal.java
+++ Tree/VerticalOrderTraversal.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.TreeMap;
+
 public class VerticalOrderTraversal {
     
     /*
@@ -32,6 +39,73 @@ public class VerticalOrderTraversal {
     Column 1: Only node 3 is in this column.
     Column 2: Only node 7 is in this column.
     */
+
+    class Pair{
+    TreeNode node;
+    int row, col;
+
+    Pair(TreeNode node, int row, int col){
+        this.node = node;
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        Queue<Pair> q = new LinkedList<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        q.offer(new Pair(root, 0, 0));
+
+        while(!q.isEmpty()){
+
+            for(int i = q.size() ; i > 0 ; i--){
+
+               Pair p = q.poll();
+               TreeNode currNode = p.node;
+               int x = p.row;
+               int y = p.col;
+
+               if(!map.containsKey(x))
+                    map.put(x, new TreeMap<>());
+
+                if(!map.get(x).containsKey(y))
+                    map.get(x).put(y, new PriorityQueue<>());
+
+                map.get(x).get(y).offer(currNode.val);
+
+               if(currNode.left != null){
+                   q.offer(new Pair(currNode.left, x - 1, y + 1));
+               }
+
+               if(currNode.right != null){
+                   q.offer(new Pair(currNode.right, x + 1, y + 1));
+               }
+            }
+        }
+
+        System.out.print(map);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(TreeMap<Integer, PriorityQueue<Integer>> key : map.values()){
+            List<Integer> currList = new ArrayList<>();
+
+            for(PriorityQueue<Integer> nodes : key.values()){
+                while(!nodes.isEmpty()){
+                    currList.add(nodes.poll());
+                }
+            }
+            System.out.print(currList);
+            ans.add(new ArrayList<>(currList));
+
+        }
+    
+        return ans;
+        
+    }
+}
 
     public class TreeNode {
 
