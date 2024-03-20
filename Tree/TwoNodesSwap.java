6099cf2c932f4b8f69b6fa1f540cb6e2ca3ef37e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-20 19:57:19 +0530

    Added TwoNodesSwap code

--- Tree/TwoNodesSwap.java
+++ Tree/TwoNodesSwap.java
@@ -1,9 +1,51 @@
+import org.w3c.dom.Node;
+
 public class TwoNodesSwap {
-    
+
     /*
-    Given the root of a Binary Search Tree with two of it's nodes swapped, find the swapped node and
-    restore the original tree.
-    */
+     * Given the root of a Binary Search Tree with two of it's nodes swapped, find
+     * the swapped node and
+     * restore the original tree.
+     */
+
+    TreeNode first;
+    TreeNode second;
+    TreeNode prev;
+
+    public void helper(Node root) {
+        inOrderTraversal(root);
+
+        // swap
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        if (prev != null && prev.val > node.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = node;
+        }
+        prev = node;
+        inOrderTraversal(node.right);
+    }
+
+    public void printIOT(Node node) {
+        if (node == null) {
+            return;
+        }
+        printIOT(node.left);
+        System.out.print(node.val + " ");
+        printIOT(node.right);
+    }
 
     public class TreeNode {
 
@@ -16,12 +58,6 @@ public class TwoNodesSwap {
 
         TreeNode(int val) {
             this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
         }
     }
 }
