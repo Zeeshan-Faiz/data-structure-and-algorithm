22ac302e8cf7955608b5313d99050d3737ae7a45
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-20 19:55:47 +0530

    Added TwoNodesSwap code

--- Tree/TwoNodesSwap.java
+++ Tree/TwoNodesSwap.java
@@ -4,4 +4,24 @@ public class TwoNodesSwap {
     Given the root of a Binary Search Tree with two of it's nodes swapped, find the swapped node and
     restore the original tree.
     */
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
