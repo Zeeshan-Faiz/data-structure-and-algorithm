5358a768c33bf984d5ddf8a02a296cf677d4a089
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-20 19:36:30 +0530

    Added ConvertBSTtoDLL code

---
+++ Tree/ConvertBSTtoDLL.java
@@ -0,0 +1,23 @@
+public class ConvertBSTtoDLL {
+    
+}
+
+class DLLNode {
+    int val;
+    DLLNode prev;
+    DLLNode next;
+  
+    public DLLNode (int val) {
+      this.val = val;
+    }
+  }
+  
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+  
+    public TreeNode (int val) {
+      this.val = val;
+    }
+  }
