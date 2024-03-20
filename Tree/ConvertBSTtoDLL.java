e0681abba7dd8a443452b937a4cdca1524ab6232
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-20 19:39:04 +0530

    Added ConvertBSTtoDLL code

--- Tree/ConvertBSTtoDLL.java
+++ Tree/ConvertBSTtoDLL.java
@@ -1,26 +1,60 @@
 public class ConvertBSTtoDLL {
-    
+
     /*
-    You are given the root of a Binary Search Tree, convert the tree into a doubly linked list.
+     * You are given the root of a Binary Search Tree, convert the tree into a doubly linked list.
     */
+
+    DLLNode head;
+    DLLNode tail;
+
+    public TreeNode convert(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        helper(root);
+        return root;
+    }
+
+    private void helper(TreeNode node) {
+        
+        if(node == null) 
+          return;
+        
+        //inorder traversal
+        helper(node.left);
+
+        DLLNode newNode = new DLLNode(node.val);
+    
+        if(head == null) {
+          head = newNode;
+          tail = newNode;
+        } 
+        else {
+          tail.next = newNode;
+          newNode.prev = tail;
+          tail = newNode;
+        }
+        helper(node.right);
+    }
 }
 
 class DLLNode {
     int val;
     DLLNode prev;
     DLLNode next;
-  
-    public DLLNode (int val) {
-      this.val = val;
+
+    public DLLNode(int val) {
+        this.val = val;
     }
-  }
-  
-  class TreeNode {
+}
+
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-  
-    public TreeNode (int val) {
-      this.val = val;
+
+    public TreeNode(int val) {
+        this.val = val;
     }
-  }
+}
