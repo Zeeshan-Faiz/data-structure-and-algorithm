b79b88c33d1659d995ef7bddee07cdad456dbb22
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-20 18:01:50 +0530

    Added KthSmallestElementApproach2 code

--- Tree/KthSmallestElementApproach2.java
+++ Tree/KthSmallestElementApproach2.java
@@ -1,41 +1,39 @@
-import javax.swing.tree.TreeNode;
-
 public class KthSmallestElementApproach2 {
-    
+
     /*
-    Given the root of a binary search tree, and an integer k, return the kth smallest value 
-    (1-indexed) of all the values of the nodes in the tree.
-
-    Example 1:
-    Input: root = [3,1,4,null,2], k = 1
-    Output: 1
-
-    Example 2:
-    Input: root = [5,3,6,2,4,null,null,1], k = 3
-    Output: 3
+     * Given the root of a binary search tree, and an integer k, return the kth
+     * smallest value (1-indexed) of all the values of the nodes in the tree.
+     
+     * Example 1:
+     * Input: root = [3,1,4,null,2], k = 1
+     * Output: 1
+      
+     * Example 2:
+     * Input: root = [5,3,6,2,4,null,null,1], k = 3
+     * Output: 3
     */
 
     private int k;
     private int ans;
-  
+
     public int kthSmallest(TreeNode root, int k) {
-            this.k = k;
-            helper(root);
-            return ans;
-        }
+        this.k = k;
+        helper(root);
+        return ans;
+    }
 
     private void helper(TreeNode node) {
-        if (node == null) {
+        
+        if (node == null) 
             return;
-        }
 
-        helper(node.left);
-
+        helper(node.left);//inorder traversal
         k--;
-        if(k==0) {
+        if (k == 0) {
             ans = node.val;
             return;
         }
+        helper(node.right);
     }
 
     public class TreeNode {
