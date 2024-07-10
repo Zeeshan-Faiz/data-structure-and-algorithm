57082c69aae9f4d34974dedc3d62bde633c4869c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-10 18:13:17 +0530

    Added LongestCommonSubsequenceTTB code

--- DynamicProgramming/LongestCommonSubsequenceTTB.java
+++ DynamicProgramming/LongestCommonSubsequenceTTB.java
@@ -1,3 +1,26 @@
+/*
+Given two strings of length m and n, find the longest common subsequence between them.
+*/
+
 public class LongestCommonSubsequenceTTB {
     
+
+    public static void main(String args[])
+    {
+
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+
+        int m = X.length();
+        int n = Y.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println("Length of LCS is "
+                           + lcs(X, Y, m, n, dp));
+    }
 }
