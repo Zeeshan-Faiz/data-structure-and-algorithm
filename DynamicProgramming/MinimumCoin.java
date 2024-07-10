181ef6614f9886ece19e5340534d71673d7ee2bc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-10 14:34:07 +0530

    Added MinimumCoin code

--- DynamicProgramming/MinimumCoin.java
+++ DynamicProgramming/MinimumCoin.java
@@ -1,3 +1,24 @@
+/*
+You are given a set of coins (1,5,7) and a target 18, Calculate the minimum number of coins required
+to reach the target.
+*/
+
 public class MinimumCoin{
-    
+
+    public static void main(String[] args) {
+
+		int n = 18;
+		int a[] = {7, 5, 1};
+		
+		int dp[] = new int[n+1];
+		Arrays.fill(dp, -1);
+		dp[0] = 0;
+		
+		int ans = minCoins(n, a, dp);
+		System.out.println(ans);
+		
+		for(int x: dp) {
+			System.out.print(x+" ");
+		}
+	}
 }
