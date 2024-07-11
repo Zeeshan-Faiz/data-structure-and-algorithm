2d874576a0d62a4a84a945dffa1da239192949c5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-11 11:11:48 +0530

    Added ConvertString code

--- DynamicProgramming/ConvertString.java
+++ DynamicProgramming/ConvertString.java
@@ -8,6 +8,30 @@ s2 = DIG
 
 O/P = 1
 */
-public class ConvertString{
+public class ConvertString {
 
+    public int minOperations(String s1, String s2) {
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = j;
+
+        for(int i = 0; i <=m; i++){
+            for(int j = 0; j <=n; j++){
+
+                //rule 1 : If characters are same
+                if(s1.charAt(i) == s2.charAt(j))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j]),dp[i-1][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
 }
