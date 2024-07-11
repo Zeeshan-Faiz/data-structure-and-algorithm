f9e59e0096aa9342037ea82969dced6397ae53e8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-11 12:14:16 +0530

    Added MaxSumSubMatrix code

--- DynamicProgramming/MaxSumSubMatrix.java
+++ DynamicProgramming/MaxSumSubMatrix.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 
 public class MaxSumSubMatrix {
 
-    public int maxSumSubMatrix(int R, int C, int[][] M){
+    public int maxSumSubMatrix(int R, int C, int[][] M) {
 
         int[] sum = new int[R];
         int maxSum = Integer.MIN_VALUE;
-        for(int cStart = 0; cStart < C; cStart ++){
+        for (int cStart = 0; cStart < C; cStart++) {
             Arrays.fill(sum, 0);
-            for(int cEnd = cStart; cEnd < C; cEnd ++){
-                for(int row = 0; row < R; row ++){
+            for (int cEnd = cStart; cEnd < C; cEnd++) {
+                for (int row = 0; row < R; row++) {
                     sum[row] += M[row][cEnd];
                 }
                 int currMaxSum = kadans(sum);
@@ -23,15 +23,15 @@ public class MaxSumSubMatrix {
         return maxSum;
     }
 
-    int kadans(int[] a){
-        
+    int kadans(int[] a) {
+
         int size = a.length;
         int max = Integer.MIN_VALUE, max_end = 0;
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             max_end = max_end + a[i];
-            if(max < max_end)
+            if (max < max_end)
                 max = max_end;
-            if(max_end < 0)
+            if (max_end < 0)
                 max_end = 0;
         }
         return max;
