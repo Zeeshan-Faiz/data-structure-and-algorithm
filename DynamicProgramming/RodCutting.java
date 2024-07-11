d006251d1ce5abdec0ad76bfb5a86caa4086653c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-11 11:43:54 +0530

    Added RodCutting code

--- DynamicProgramming/RodCutting.java
+++ DynamicProgramming/RodCutting.java
@@ -1,3 +1,15 @@
+/*
+Given the prices of different length of rods, find the maximum profit that can be generated for any
+given length.
+
+Example :
+lenght : 1 2 3 4  5  6  7  8
+prices : 1 5 6 9 11 12 14 16
+
+Length = 8,
+max profit = 20
+*/
+
 public class RodCutting {
     
 }
