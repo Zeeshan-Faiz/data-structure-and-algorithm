422e2a22f6e5524bdc0ce0133de24a7780b5f92f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-11 11:45:53 +0530

    Added RodCutting code

--- DynamicProgramming/RodCutting.java
+++ DynamicProgramming/RodCutting.java
@@ -11,5 +11,15 @@ max profit = 20
 */
 
 public class RodCutting {
-    
+
+    static int cutRod(int[] price, int n) {
+
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], price[j] + dp[i - j - 1]);
+            }
+        }
+        return dp[n];
+    }
 }
