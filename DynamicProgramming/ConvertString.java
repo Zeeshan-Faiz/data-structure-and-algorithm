8aa8dd285f31d0a9a8e6b11b00f7ba8a95a738f2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-11 11:07:11 +0530

    Added ConvertString code

--- DynamicProgramming/ConvertString.java
+++ DynamicProgramming/ConvertString.java
@@ -1,3 +1,13 @@
+/*
+You are given two strings s1 and s2, find the minimum number of operations to convert String s1 to
+s2. (Opearations available -->Insert,Delete,Replace)
+
+Example :
+s1 = DOG
+s2 = DIG
+
+O/P = 1
+*/
 public class ConvertString{
-    
+
 }
