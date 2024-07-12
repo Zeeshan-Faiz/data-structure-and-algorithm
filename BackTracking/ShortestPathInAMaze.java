e29503cb09e687fddca6cdf008eed39b2999aed9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-12 10:35:59 +0530

    Added ShortestPathInAMaze code

--- BackTracking/ShortestPathInAMaze.java
+++ BackTracking/ShortestPathInAMaze.java
@@ -1,3 +1,8 @@
+/*
+Given a source and a destination in a maze, return the length of the shortest path from soure to
+destination. (NOTE: You can only traverse through 1's and not 0's) 
+*/
+
 public class ShortestPathInAMaze {
     
 }
