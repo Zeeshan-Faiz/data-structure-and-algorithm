5c60c5abe3d07b4c4f764ba99c86c13752bc054a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-12 10:41:49 +0530

    Added ShortestPathInAMaze code

--- BackTracking/ShortestPathInAMaze.java
+++ BackTracking/ShortestPathInAMaze.java
@@ -15,6 +15,30 @@ public class ShortestPathInAMaze {
     }
 
     static boolean isValid(int[][] a, int i, int j, boolean[][] vis){
-        
+
+        int rows = a.length;
+        int cols = a[0].length;
+
+        return i >= 0 && j >= 0 && i < rows && j < cols && a[i][j] == 1 && !vis[i][j];
+    }
+
+    static int helper(int[][] a, int i, int j, int x, int y, boolean[][] vis){
+
+        if(!isValid(a, i, j, vis))
+            return 1000000;
+
+        if(i == x && j == y)
+            return 0;
+
+        vis[i][j] = true;
+        int left = helper(a, i, j-1, x, y, vis);
+        int bottom = helper(a, i+1, j, x, y, vis);
+        int right = helper(a, i, j+1, x, y, vis);
+        int top = helper(a, i-1, j, x, y, vis);
+
+        //backtract
+        vis[i][j] = false;
+
+        return Math.min(Math.min(left, bottom), Math.min(right, top));
     }
 }
