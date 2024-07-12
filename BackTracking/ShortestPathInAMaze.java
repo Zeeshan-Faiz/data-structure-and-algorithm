896f39a73b097cfc8955fe2bef0a744cbb7e1163
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-12 10:37:40 +0530

    Added ShortestPathInAMaze code

--- BackTracking/ShortestPathInAMaze.java
+++ BackTracking/ShortestPathInAMaze.java
@@ -5,4 +5,16 @@ destination. (NOTE: You can only traverse through 1's and not 0's)
 
 public class ShortestPathInAMaze {
     
+    static int shortestPath(int[][] a, int i, int j, int x, int y){
+
+        int rows = a.length;
+        int cols = a[0].length;
+
+        boolean[][] vis = new boolean[rows][cols];
+        return helper(a,i,j,x,y,vis);
+    }
+
+    static boolean isValid(int[][] a, int i, int j, boolean[][] vis){
+        
+    }
 }
