013c2ae8e2416e7661859e8dc99855c092eae7c9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-13 20:29:05 +0530

    Added DetectCycleInDirectedGraph code

--- Graphs/DetectCycleInDirectedGraph.java
+++ Graphs/DetectCycleInDirectedGraph.java
@@ -1,3 +1,8 @@
+/*
+You are given the adjacency list containg the vertices of a Directed graph, check whether the graph contains
+cycle or not.
+*/
+
 public class DetectCycleInDirectedGraph {
     
 }
