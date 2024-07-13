00ec924648312151f372b599eded765b9f26dbac
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-13 15:43:58 +0530

    Added DetectCycleInUndirectedGraph code

--- Graphs/DetectCycleInUndirectedGraph.java
+++ Graphs/DetectCycleInUndirectedGraph.java
@@ -6,17 +6,28 @@ cycle or not.
 import java.util.ArrayList;
 
 public class DetectCycleInUndirectedGraph {
-    
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
 
         boolean[] vis = new boolean[V];
-        for(int i = 0; i < V; i++){
-            if(!vis[i])
-                if(dfs(i,adj,vis,-1))
+        for (int i = 0; i < V; i++) {
+            if (!vis[i])
+                if (dfs(i, adj, vis, -1))
                     return true;
         }
         return false;
     }
 
-    
+    private boolean dfs(int v, ArrayList<ArrayList<Integer>> adj, boolean[] vis, int parent) {
+
+        vis[v] = true;
+        for (Integer neighbor : adj.get(v)) {
+            if (!vis[neighbor]) {
+                if (dfs(neighbor, adj, vis, v))
+                    return true;
+            } else if (parent != neighbor)
+                return true;
+        }
+        return false;
+    }
 }
