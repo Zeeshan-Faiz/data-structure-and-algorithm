5adf044c363dcd1d7dfd9b5bb9c3a872d1ae0fb8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-13 15:42:15 +0530

    Added DetectCycleInUndirectedGraph code

--- Graphs/DetectCycleInUndirectedGraph.java
+++ Graphs/DetectCycleInUndirectedGraph.java
@@ -2,6 +2,21 @@
 You are given the adjacency list containg the vertices of a graph, check whether the graph contains
 cycle or not.
 */
+
+import java.util.ArrayList;
+
 public class DetectCycleInUndirectedGraph {
+    
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+
+        boolean[] vis = new boolean[V];
+        for(int i = 0; i < V; i++){
+            if(!vis[i])
+                if(dfs(i,adj,vis,-1))
+                    return true;
+        }
+        return false;
+    }
+
     
 }
