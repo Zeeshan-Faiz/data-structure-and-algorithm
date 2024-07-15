68da993b428593bf6641785f649d3fd6f2e0ff6e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 19:47:26 +0530

    Added MinSpanningTree_PrimsAlgorithm code

--- Graphs/MinSpanningTree_PrimsAlgorithm.java
+++ Graphs/MinSpanningTree_PrimsAlgorithm.java
@@ -1,3 +1,8 @@
+/*
+Given a graph with vertices and each edges between vertices has some weights, find the minimum weight
+that we can get even when all vertices are connected to each other.
+*/
+
 public class MinSpanningTree_PrimsAlgorithm {
     
 }
