8f5965860226818bb7de2686dbf7b7a349efcc02
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 19:50:32 +0530

    Added MinSpanningTree_PrimsAlgorithm code

--- Graphs/MinSpanningTree_PrimsAlgorithm.java
+++ Graphs/MinSpanningTree_PrimsAlgorithm.java
@@ -3,18 +3,37 @@ Given a graph with vertices and each edges between vertices has some weights, fi
 that we can get even when all vertices are connected to each other.
 */
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class MinSpanningTree_PrimsAlgorithm {
-    
-    class Pair implements Comparable<Pair>{
+
+    class Pair implements Comparable<Pair> {
 
         int wt;
         int v;
-        Pair(int v, int wt){
+
+        Pair(int v, int wt) {
             this.v = v;
             this.wt = wt;
         }
-        public int compareTo(Pair that){
+
+        public int compareTo(Pair that) {
             return this.wt - that.wt;
+        }
+    }
+
+    static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) {
+
+        boolean[] vis = new boolean[V];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        pq.add(new Pair(0, 0));
+        int ans = 0;
+        while (!pq.isEmpty()) {
+
+            Pair cur = pq.remove();
+
         }
     }
 }
