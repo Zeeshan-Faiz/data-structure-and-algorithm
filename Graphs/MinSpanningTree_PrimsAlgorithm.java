87fd2cc60e657130df9bbd611f9e0f7c527dbf6a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 19:48:56 +0530

    Added MinSpanningTree_PrimsAlgorithm code

--- Graphs/MinSpanningTree_PrimsAlgorithm.java
+++ Graphs/MinSpanningTree_PrimsAlgorithm.java
@@ -5,4 +5,16 @@ that we can get even when all vertices are connected to each other.
 
 public class MinSpanningTree_PrimsAlgorithm {
     
+    class Pair implements Comparable<Pair>{
+
+        int wt;
+        int v;
+        Pair(int v, int wt){
+            this.v = v;
+            this.wt = wt;
+        }
+        public int compareTo(Pair that){
+            return this.wt - that.wt;
+        }
+    }
 }
