c77f02b93ef78335cdfe34d69b9ea71ddd9e566e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 20:22:53 +0530

    Added KrushKalsAlgorithm code

--- Graphs/KrushKalsAlgorithm.java
+++ Graphs/KrushKalsAlgorithm.java
@@ -1,8 +1,24 @@
 /*
-Given a Connected, weighted and Un-directed grapg, find the minimum spanning tree in this graph using
+Gidesten a Connected, weighted and Un-directed grapg, find the minimum spanning tree in this graph using
 Krushkal's Algorithm.
 */
 
 public class KrushKalsAlgorithm {
-    
+
+    public class Edges implements Comparable<Edges> {
+
+        int src;
+        int dest;
+        int wt;
+
+        Edges(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+
+        public int compareTo(Edges that) {
+            return this.wt - that.wt;
+        }
+    }
 }
