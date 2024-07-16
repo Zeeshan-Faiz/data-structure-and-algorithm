6c090764b9f0c5d8af017f28089aad6d6602343e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 21:02:17 +0530

    Added KrushKalsAlgorithm code

--- Graphs/KrushKalsAlgorithm.java
+++ Graphs/KrushKalsAlgorithm.java
@@ -4,42 +4,77 @@ Krushkal's Algorithm.
 */
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class KrushKalsAlgorithm {
 
-    public class Edges implements Comparable<Edges> {
+    public class Edge implements Comparable<Edge> {
 
         int src;
         int dest;
         int wt;
 
-        Edges(int src, int dest, int wt) {
+        Edge(int src, int dest, int wt) {
             this.src = src;
             this.dest = dest;
             this.wt = wt;
         }
-        public int compareTo(Edges that) {
+
+        public int compareTo(Edge that) {
             return this.wt - that.wt;
         }
 
         static int p[], rank[];
-        static void union(int x, int y){
+
+        static void union(int x, int y) {
             int rx = find(x);
             int ry = find(y);
-            if(rx == ry)
+            if (rx == ry)
                 return;
             p[ry] = rx;
         }
 
-        static int find(int x){
-            if(p[x] == x)
+        static int find(int x) {
+            if (p[x] == x)
                 return x;
             return find(p[x]);
         }
 
-        static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) {
+        int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) {
 
-        boolean[][] vis = new boolean[V][V];
-        
+            boolean[][] added = new boolean[V][V];
+            ArrayList<Edge> edges = new ArrayList<>();
+
+            for (int i = 0; i < adj.size(); i++) {
+                for (int j = 0; j < adj.get(i).size(); j++) {
+                    ArrayList<Integer> cur = adj.get(i).get(j);
+                    if (!added[i][cur.get(0)]) {
+                        added[i][cur.get(0)] = true;
+                        added[cur.get(0)][i] = true;
+                        edges.add(new Edge(i, cur.get(0), cur.get(1)));
+                    }
+                }
+            }
+
+            p = new int[V];
+            for (int i = 0; i < V; i++)
+                p[i] = i;
+            Collections.sort(edges);// sort all edges
+
+            int count = 0, ans = 0;
+            for (int i = 0; count < V; i++) {
+                Edge edge = edges.get(i);
+                int rx = find(edge.src);
+                int ry = find(edge.dest);
+                // if representative/edge are different, make them union
+                if (rx != ry) {
+                    union(rx, ry);
+                    count++;
+                    ans += edge.wt;
+                }
+            }
+            return ans;
+        }
     }
 }
