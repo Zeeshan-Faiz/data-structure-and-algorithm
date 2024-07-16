16166d4678a616f23255ec61e393b002f6b71eee
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 16:53:07 +0530

    Added BellmanFordAlgorithm code

--- Graphs/BellmanFordAlgorithm.java
+++ Graphs/BellmanFordAlgorithm.java
@@ -1,3 +1,8 @@
+/*
+Given a graph with vertices and each edge between vertices has some weights(here we can have negative weights as well), 
+Find whether the graph contains Negative Weight Cycle.
+*/
+
 public class BellmanFordAlgorithm {
     
 }
