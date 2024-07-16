83cb4876f70c8a26c7bdcb8bb64463931f933e0f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 15:09:02 +0530

    Added DijkstrasAlgorithm code

--- Graphs/DijkstrasAlgorithm.java
+++ Graphs/DijkstrasAlgorithm.java
@@ -5,4 +5,16 @@ from source to all other vertices.(Use Dijkstra's Algorithm)
 
 public class DijkstrasAlgorithm {
     
+    public class Pair implements Comparable<Pair> {
+
+        int wt;
+        int v;
+        Pair(int v, int wt) {
+            this.v = v;
+            this.wt = wt;
+        }
+        public int compareTo(Pair that) {
+            return this.wt - that.wt;
+        }
+    }
 }
